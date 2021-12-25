d55aec920a5e5d47aa702a8d44e8194babe34b76
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2021-12-25 20:29:28 +0100

    Add jest to monorepo

---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+import baseConfig from '../jest.base.config.js';
+
+export default {
+    ...baseConfig,
+    name: 'backend',
+    displayName: 'Backend',
+    roots: ['<rootDir>'],
+};
---
+++ shared/jest.config.js
@@ -0,0 +1,8 @@
+import baseConfig from '../jest.base.config.js';
+
+export default {
+    ...baseConfig,
+    name: 'shared',
+    displayName: 'Shared',
+    roots: ['<rootDir>'],
+};
