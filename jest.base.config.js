a9cf65418a07658a6ac1dbf9d6e5f468ec4b5289
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2021-12-29 14:55:50 +0100

    Add verbose flag

--- jest.base.config.js
+++ jest.base.config.js
@@ -4,7 +4,6 @@
  * Everything related to the code coverage has to be specified in the global config. https://github.com/facebook/jest/issues/4255#issuecomment-321939025
  */
 export default {
-    verbose: true,
     moduleFileExtensions: ['js', 'json', 'ts'],
     transform: {
         '^.+\\.ts$': 'ts-jest',
