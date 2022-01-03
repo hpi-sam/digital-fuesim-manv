7c69af477b793fd6509d9fbfe3181aa3cd43d6a3
Julian Schmidt <18506183+Dassderdie@users.noreply.github.com>
2022-01-03 22:40:46 +0100

    Remove not working JSON types and replace them with comments

--- shared/src/utils/immutability.ts
+++ shared/src/utils/immutability.ts
@@ -1,7 +1,9 @@
-// TODO: we only want to use Immutable and Mutable for JsonObjects (not for Maps, Sets, Functions, ...)
+// TODO: Add typings for JsonObjects
 /**
  * An immutable object is an object whose state cannot be modified.
  * Makes all properties in the given object readonly (recursive).
+ *
+ * The provided object should be a JSON object (no Maps, Sets, Functions, ...).
  */
 export type Immutable<T extends object> = {
     readonly [K in keyof T]: T[K] extends object ? Immutable<T[K]> : T[K];
@@ -10,18 +12,9 @@ export type Immutable<T extends object> = {
 /**
  * A mutable object is an object whose state can be modified.
  * Removes all readonly modifiers from the given object (recursive).
+ *
+ * The provided object should be a JSON object (no Maps, Sets, Functions, ...).
  */
 export type Mutable<T extends object> = {
     -readonly [K in keyof T]: T[K] extends object ? Mutable<T[K]> : T[K];
 };
-
-// TODO: Do we need this?
-export interface JsonObject {
-    [key: string]: JsonObject | JsonObject[] | JsonPrimitive | JsonPrimitive[];
-}
-
-/**
- * `undefined` is not a valid JSON value. You have to either use `null` or remove the property key from the object.
- * JSON.stringify() will (by default) treat all properties with `undefined` values as not existing.
- */
-type JsonPrimitive = boolean | number | string | null;
