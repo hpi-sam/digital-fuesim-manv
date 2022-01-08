3d8e98c25e78632911ddd9c7f169b6c9d399a028
Julian Schmidt <18506183+Dassderdie@users.noreply.github.com>
2022-01-08 17:35:49 +0100

    Fix an incorrect import and apply prettier

--- frontend/src/app/state/app.reducer.ts
+++ frontend/src/app/state/app.reducer.ts
@@ -1,12 +1,7 @@
 import type { ActionReducerMap } from '@ngrx/store';
-import type { ExerciseState } from 'digital-fuesim-manv-shared';
-import { exerciseReducer } from 'digital-fuesim-manv-shared';
 import type { AppState } from './app.state';
+import { exerciseReducer } from './exercise/exercise.reducer';
 
 export const appReducers: ActionReducerMap<AppState> = {
-    // TODO: I don't know why the `ActionReducer` type from the reducer is not working here
-    exercise: exerciseReducer as (
-        state: ExerciseState | undefined,
-        action: any
-    ) => ExerciseState,
+    exercise: exerciseReducer,
 };
