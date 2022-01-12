529c19cfe0dbf28ea3126293da2b485848bebc80
Julian Schmidt <18506183+Dassderdie@users.noreply.github.com>
2022-01-13 00:00:31 +0100

    Refactor movementAnimations to own class * Fixed multiple edge cases

---
+++ frontend/src/app/shared/exercise-map/renderers/movement-animator.ts
@@ -0,0 +1,113 @@
+import type Geometry from 'ol/geom/Geometry';
+import type VectorLayer from 'ol/layer/Vector';
+import type VectorSource from 'ol/source/Vector';
+import type OlMap from 'ol/Map';
+import type RenderEvent from 'ol/render/Event';
+import type { Feature } from 'ol';
+import { getVectorContext } from 'ol/render';
+import type Circle from 'ol/geom/Circle';
+import type { UUID } from 'digital-fuesim-manv-shared';
+import { isEqual } from 'lodash-es';
+
+export class MovementAnimator {
+    /**
+     * The time in milliseconds how long the moving animation should take
+     */
+    private readonly movementAnimationTime = 200;
+
+    constructor(
+        private readonly olMap: OlMap,
+        private readonly layer: VectorLayer<VectorSource<Geometry>>
+    ) {}
+
+    /**
+     * This map stores the listeners for each feature to be able to unsubscribe them.
+     */
+    private readonly animationListeners = new Map<
+        UUID,
+        (event: RenderEvent) => void
+    >();
+
+    /**
+     * This method animates the movement of the feature from its current position to the given end position
+     * by linearly interpolating it's position over the next {@link movementAnimationTime} milliseconds.
+     *
+     * If the feature is removed from the layer before the animation is finished, it is recommended to call {@link stopMovementAnimation}.
+     */
+    public animateFeatureMovement(
+        feature: Feature<Circle>,
+        endPosition: [number, number]
+    ) {
+        const startTime = Date.now();
+        const featureGeometry = feature.getGeometry()!;
+        const startPosition = featureGeometry.getCenter() as [number, number];
+        // Stop an ongoing movement animation
+        this.stopMovementAnimation(feature);
+        // We don't have to animate this
+        if (isEqual(startPosition, endPosition)) {
+            return;
+        }
+        const listener = (event: RenderEvent) => {
+            this.animationTick(
+                event,
+                startTime,
+                startPosition,
+                endPosition,
+                feature
+            );
+        };
+        this.animationListeners.set(this.getFeatureId(feature), listener);
+        // The listener unsubscribes itself
+        this.layer.on('postrender', listener);
+        // Trigger the first animation tick
+        this.olMap.render();
+    }
+
+    /**
+     * This method should be called after each postrender event.
+     * It sets the feature to the next interpolated position.
+     */
+    private animationTick(
+        event: RenderEvent,
+        startTime: number,
+        startPosition: [number, number],
+        endPosition: [number, number],
+        feature: Feature<Circle>
+    ) {
+        const featureGeometry = feature.getGeometry()!;
+        const elapsedTime = event.frameState!.time - startTime;
+        // A value between 0 and 1
+        const progress = Math.max(
+            Math.min(elapsedTime / this.movementAnimationTime, 1),
+            0
+        );
+        // We should already be (nearly) at the end position
+        if (progress >= 1) {
+            this.stopMovementAnimation(feature);
+            featureGeometry.setCenter(endPosition);
+            return;
+        }
+        // The next position is calculated by a linear interpolation between the start and end position
+        const nextPosition = [
+            startPosition[0] + (endPosition[0] - startPosition[0]) * progress,
+            startPosition[1] + (endPosition[1] - startPosition[1]) * progress,
+        ];
+        featureGeometry.setCenter(nextPosition);
+        getVectorContext(event).drawGeometry(featureGeometry);
+        this.olMap.render();
+    }
+
+    public stopMovementAnimation(feature: Feature<Circle>) {
+        const listenerId = this.getFeatureId(feature);
+        if (!this.animationListeners.has(listenerId)) {
+            return;
+        }
+        this.layer.un('postrender', this.animationListeners.get(listenerId!)!);
+        this.animationListeners.delete(listenerId);
+    }
+
+    private getFeatureId(feature: Feature<Circle>) {
+        // TODO: handle features without an id
+        return feature.getId()! as UUID;
+    }
+}
--- frontend/src/app/shared/exercise-map/renderers/patient-renderer.ts
+++ frontend/src/app/shared/exercise-map/renderers/patient-renderer.ts
@@ -5,13 +5,17 @@ import type Geometry from 'ol/geom/Geometry';
 import type VectorLayer from 'ol/layer/Vector';
 import type VectorSource from 'ol/source/Vector';
 import type { ApiService } from 'src/app/core/api.service';
-import { getVectorContext } from 'ol/render';
 import type OlMap from 'ol/Map';
-import type RenderEvent from 'ol/render/Event';
 import { hasAPropertyChanged } from '../utility/has-a-property-changed';
 import { ElementRenderer } from './element-renderer';
+import { MovementAnimator } from './movement-animator';
 
 export class PatientRenderer extends ElementRenderer<Patient> {
+    private readonly movementAnimator = new MovementAnimator(
+        this.olMap,
+        this.patientLayer
+    );
+
     constructor(
         private readonly olMap: OlMap,
         private readonly patientLayer: VectorLayer<VectorSource<Geometry>>,
@@ -48,9 +52,9 @@ export class PatientRenderer extends ElementRenderer<Patient> {
     }
 
     public deleteElement(patient: Patient): void {
-        this.patientLayer
-            .getSource()
-            .removeFeature(this.getPatientFeature(patient));
+        const patientFeature = this.getPatientFeature(patient);
+        this.patientLayer.getSource().removeFeature(patientFeature);
+        this.movementAnimator.stopMovementAnimation(patientFeature);
     }
 
     public override changeElement(
@@ -67,10 +71,10 @@ export class PatientRenderer extends ElementRenderer<Patient> {
                 this.deleteElement(oldPatient);
                 return;
             }
-            this.animateFeatureMovement(this.getPatientFeature(newPatient), [
-                newPatientPosition.x,
-                newPatientPosition.y,
-            ]);
+            this.movementAnimator.animateFeatureMovement(
+                this.getPatientFeature(newPatient),
+                [newPatientPosition.x, newPatientPosition.y]
+            );
             return;
         }
         super.changeElement(oldPatient, newPatient);
@@ -80,63 +84,5 @@ export class PatientRenderer extends ElementRenderer<Patient> {
         return this.patientLayer
             .getSource()
             .getFeatureById(patient.id) as Feature<Circle>;
-    }
-
-    /**
-     * The time in milliseconds how long the moving animation should take
-     */
-    private readonly movementAnimationTime = 100;
-
-    private animateFeatureMovement(
-        feature: Feature<Circle>,
-        endPosition: [number, number]
-    ) {
-        const startTime = Date.now();
-        const featureGeometry = feature.getGeometry()!;
-        const startPosition = featureGeometry.getCenter() as [number, number];
-        const listener = (event: RenderEvent) => {
-            this.animationTick(
-                event,
-                startTime,
-                startPosition,
-                endPosition,
-                featureGeometry
-            );
-        };
-        this.patientLayer.on('postrender', listener);
-        this.olMap.render();
-        // TODO: remove listener when another movement animation for it is started
-        setTimeout(() => {
-            this.patientLayer.un('postrender', listener);
-            // Sometimes the position is not correctly updated in the postrender listener
-            featureGeometry.setCenter(endPosition);
-        }, this.movementAnimationTime);
-    }
-
-    private animationTick(
-        event: RenderEvent,
-        startTime: number,
-        startPosition: [number, number],
-        endPosition: [number, number],
-        featureGeometry: Circle
-    ) {
-        const elapsedTime = event.frameState!.time - startTime;
-        // A value between 0 and 1
-        const progress = Math.max(
-            Math.min(elapsedTime / this.movementAnimationTime, 1),
-            0
-        );
-        // We should already be (nearly) at the end position
-        if (progress >= 1) {
-            return;
-        }
-        // The next position is calculated by a linear interpolation between the start and end position
-        const nextPosition = [
-            startPosition[0] + (endPosition[0] - startPosition[0]) * progress,
-            startPosition[1] + (endPosition[1] - startPosition[1]) * progress,
-        ];
-        featureGeometry.setCenter(nextPosition);
-        getVectorContext(event).drawGeometry(featureGeometry);
-        this.olMap.render();
     }
 }
