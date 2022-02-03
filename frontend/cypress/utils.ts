48f1a8e2e9dea5a67d33d32b1c172eafaa8c62a8
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-02-03 16:18:38 +0100

    Add withExercise

---
+++ frontend/cypress/utils.ts
@@ -0,0 +1,19 @@
+import type { ExerciseIds } from 'digital-fuesim-manv-shared';
+
+export async function withExercise(
+    callback: (ids: ExerciseIds) => Promise<void>
+) {
+    cy.request<ExerciseIds>('POST', 'http://locahost:3201/api/exercise').then(
+        async (x) => {
+            const ids = x.body;
+            try {
+                await callback(ids);
+            } finally {
+                cy.request(
+                    'DELETE',
+                    `http://locahost:3201/api/exercise/${ids.trainerId}`
+                );
+            }
+        }
+    );
+}
