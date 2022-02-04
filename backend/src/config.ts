d39f561ff8b8f7da5f0d9919328ac4b46370ba00
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-02-04 15:54:40 +0100

    Use envalid for env validation

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,14 +1,8 @@
+import { cleanEnv, makeValidator } from 'envalid';
+
 class ConfigNotSetUpError extends Error {
     public constructor() {
         super('The Config is not properly set up.');
-    }
-}
-
-class InvalidConfigValue extends Error {
-    public constructor(field: string, value: any) {
-        super(
-            `Setting up config field ${field} with value \`${value}\` is not possible.`
-        );
     }
 }
 
@@ -24,14 +18,6 @@ export class Config {
         return this._websocketPort;
     }
 
-    private static setWebsocketPort(target: string) {
-        const converted = Number.parseInt(target);
-        if (!Number.isInteger(converted)) {
-            throw new InvalidConfigValue('websocketPort', target);
-        }
-        this._websocketPort = converted;
-    }
-
     public static get httpPort(): number {
         if (this._httpPort === undefined) {
             throw new ConfigNotSetUpError();
@@ -39,24 +25,29 @@ export class Config {
         return this._httpPort;
     }
 
-    private static setHttpPort(target: string) {
-        const converted = Number.parseInt(target);
-        if (!Number.isInteger(converted)) {
-            throw new InvalidConfigValue('httpPort', target);
-        }
-        this._httpPort = converted;
+    private static parseVariables() {
+        const integer = makeValidator((x) => {
+            const int = Number.parseInt(x);
+            if (!Number.isInteger(int)) {
+                throw new TypeError('Expected an integer');
+            }
+            return int;
+        });
+        return cleanEnv(process.env, {
+            DFM_WEBSOCKET_PORT: integer({ default: 3200 }),
+            DFM_WEBSOCKET_PORT_TESTING: integer({ default: 13200 }),
+            DFM_HTTP_PORT: integer({ default: 3201 }),
+            DFM_HTTP_PORT_TESTING: integer({ default: 13201 }),
+        });
     }
 
     public static initialize(testing: boolean = false) {
-        this.setWebsocketPort(
-            (testing ? process.env.DFM_WEBSOCKET_PORT_TESTING : undefined) ??
-                process.env.DFM_WEBSOCKET_PORT ??
-                '3200'
-        );
-        this.setHttpPort(
-            (testing ? process.env.DFM_HTTP_PORT_TESTING : undefined) ??
-                process.env.DFM_HTTP_PORT ??
-                '3201'
-        );
+        const env = this.parseVariables();
+        this._websocketPort = testing
+            ? env.DFM_WEBSOCKET_PORT_TESTING
+            : env.DFM_WEBSOCKET_PORT;
+        this._httpPort = testing
+            ? env.DFM_HTTP_PORT_TESTING
+            : env.DFM_HTTP_PORT;
     }
 }
