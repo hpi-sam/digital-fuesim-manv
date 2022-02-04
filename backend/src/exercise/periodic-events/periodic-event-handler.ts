bc8911f0500691a328f5afe97bbcc6678d3fbcc6
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-02-04 16:37:54 +0100

    Allow actively skipping ticks

--- backend/src/exercise/periodic-events/periodic-event-handler.ts
+++ backend/src/exercise/periodic-events/periodic-event-handler.ts
@@ -1,13 +1,15 @@
 export class PeriodicEventHandler {
     private currentState: 'paused' | 'running' = 'paused';
 
+    private currentTick: Promise<void> | undefined = undefined;
+
     /**
      *
      * @param tick The function to be called.
      * @param interval The time between calls to {@link tick} in ms.
      */
     public constructor(
-        private readonly tick: () => void,
+        private readonly tick: () => Promise<void>,
         private readonly interval: number
     ) {}
 
@@ -21,11 +23,20 @@ export class PeriodicEventHandler {
     }
 
     private tickHandler() {
-        setTimeout(() => {
-            this.tick();
+        setTimeout(async () => {
+            if (this.currentTick !== undefined) {
+                console.warn('Tick took too long! Skipping next tick.');
+                if (this.currentState === 'running') {
+                    this.tickHandler();
+                }
+                return;
+            }
+            this.currentTick = this.tick();
             if (this.currentState === 'running') {
                 this.tickHandler();
             }
+            await this.currentTick;
+            this.currentTick = undefined;
         }, this.interval);
     }
 }
