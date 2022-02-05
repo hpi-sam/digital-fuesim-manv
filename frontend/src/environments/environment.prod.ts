f0bc352972f948fccdf9f42b292a834bbf377757
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-02-05 22:49:06 +0100

    Update frontend enviornments

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
-export const environment = {
+import type { Environment } from './environment-type';
+
+export const environment: Environment = {
     production: true,
+    httpProtocol: 'http',
+    httpPort: 3201,
+    websocketProtocol: 'ws',
+    websocketPort: 3200,
 };
