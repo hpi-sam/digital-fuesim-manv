bb328ff81b3fc587848f70c59da6b220b6bfbba8
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-02-07 14:05:46 +0100

    Use tick handler as proposed by @Dassderdie

--- backend/src/exercise/periodic-events/periodic-event-handler.ts
+++ backend/src/exercise/periodic-events/periodic-event-handler.ts
@@ -1,42 +1,44 @@
+/**
+ * Makes sure that a periodic {@link tick} function is executed every {@link interval}.
+ */
 export class PeriodicEventHandler {
-    private currentState: 'paused' | 'running' = 'paused';
-
-    private currentTick: Promise<void> | undefined = undefined;
-
+    private currentTimeout?: NodeJS.Timeout;
+    // Needed in addition to currentTimeout because tick is asynchronous and there is no timeout during its execution.
+    private isPaused = false;
     /**
      *
-     * @param tick The function to be called.
-     * @param interval The time between calls to {@link tick} in ms.
+     * @param tick A potentially synchronous or asynchronous long-running function.
+     * @param interval in ms
+     * It is guaranteed that at least as much time and tried via best effort
+     * that exactly as much time as {@link interval} is between the calls of {@link tick}.
      */
     public constructor(
         private readonly tick: () => Promise<void>,
         private readonly interval: number
     ) {}
-
     public start() {
-        this.currentState = 'running';
-        this.tickHandler();
-    }
-
-    public pause() {
-        this.currentState = 'paused';
-    }
-
-    private tickHandler() {
-        setTimeout(async () => {
-            if (this.currentTick !== undefined) {
-                console.warn('Tick took too long! Skipping next tick.');
-                if (this.currentState === 'running') {
-                    this.tickHandler();
-                }
-                return;
-            }
-            this.currentTick = this.tick();
-            if (this.currentState === 'running') {
-                this.tickHandler();
-            }
-            await this.currentTick;
-            this.currentTick = undefined;
+        this.isPaused = false;
+        this.currentTimeout = setTimeout(() => {
+            this.executeTick();
         }, this.interval);
+    }
+    public pause() {
+        this.isPaused = true;
+        if (this.currentTimeout) {
+            clearTimeout(this.currentTimeout);
+            this.currentTimeout = undefined;
+        }
+    }
+    private async executeTick() {
+        const lastTickStartTime = Date.now();
+        await this.tick();
+        if (this.isPaused) {
+            return;
+        }
+        const lastTickDuration = Date.now() - lastTickStartTime;
+        const timeToWait = Math.max(this.interval - lastTickDuration, 0);
+        this.currentTimeout = setTimeout(() => {
+            this.executeTick();
+        }, timeToWait);
     }
 }
