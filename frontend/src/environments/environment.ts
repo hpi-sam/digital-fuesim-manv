0286f6b12f594b017408861b3b49e4dccbe62344
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-02-07 19:05:28 +0100

    Fix production flag in development environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 import type { Environment } from './environment-type';
 
 export const environment: Environment = {
-    production: true,
+    production: false,
     httpPort: 3201,
     websocketPort: 3200,
 };
