2cbfd12858dd7022956d1596c078d1b173b197ce
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-02-07 15:10:09 +0100

    Remove redundant check

--- backend/src/exercise/websocket-handler/propose-action-handler.ts
+++ backend/src/exercise/websocket-handler/propose-action-handler.ts
@@ -21,10 +21,7 @@ export const registerProposeActionHandler = (
             return;
         }
         // If the action contains a timestamp, refresh it to the current server time.
-        if (
-            (action as any).timestamp !== undefined &&
-            typeof (action as any).timestamp === 'number'
-        ) {
+        if ((action as any).timestamp !== undefined) {
             (action as any).timestamp = Date.now();
         }
         // 2. TODO: validate user permissions
