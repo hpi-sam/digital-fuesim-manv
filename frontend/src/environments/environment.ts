e92f085af2dfb127da621621fa33cb1a24838275
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-02-07 10:29:26 +0100

    Automatically determine the protocoll

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,9 +2,7 @@ import type { Environment } from './environment-type';
 
 export const environment: Environment = {
     production: true,
-    httpProtocol: 'http',
     httpPort: 3201,
-    websocketProtocol: 'ws',
     websocketPort: 3200,
 };
 
