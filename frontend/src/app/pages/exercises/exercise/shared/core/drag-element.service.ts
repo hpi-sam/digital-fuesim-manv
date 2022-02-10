ed642fcc3f5f5108861d8ce1ea6473ddb782f8a4
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-02-10 23:09:15 +0100

    Resize preview image during drag to current zoom

--- frontend/src/app/pages/exercises/exercise/shared/core/drag-element.service.ts
+++ frontend/src/app/pages/exercises/exercise/shared/core/drag-element.service.ts
@@ -7,6 +7,9 @@ import type {
 import { Patient, addVehicle } from 'digital-fuesim-manv-shared';
 import { ApiService } from 'src/app/core/api.service';
 import type OlMap from 'ol/Map';
+import { ImageStyleHelper } from '../exercise-map/utility/get-image-style-function';
+import { VehicleFeatureManager } from '../exercise-map/feature-managers/vehicle-feature-manager';
+import { PatientFeatureManager } from '../exercise-map/feature-managers/patient-feature-manager';
 
 @Injectable({
     providedIn: 'root',
@@ -17,6 +20,11 @@ import type OlMap from 'ol/Map';
 export class DragElementService {
     private olMap?: OlMap;
     private readonly dragElementKey = 'createElement';
+
+    private readonly normalizedImageHeights = {
+        vehicle: VehicleFeatureManager.normalizedImageHeight,
+        patient: PatientFeatureManager.normalizedImageHeight,
+    };
 
     constructor(private readonly apiService: ApiService) {}
 
@@ -29,23 +37,59 @@ export class DragElementService {
     }
 
     public onDragStart(event: DragEvent, transferTemplate: TransferTemplate) {
+        // Create the drag image
         const dragImage = new Image();
         dragImage.src = transferTemplate.template.imageUrl;
-        // TODO: use the zoom to scale the image correctly
-        // See https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/setDragImage
-        // Changing the dragImage to another element causes nondeterministic behavior
-        // const zoom = this.olMap!.getView().getZoom()!;
-        event.dataTransfer?.setDragImage(
-            dragImage,
-            dragImage.width / 2,
-            dragImage.height / 2
-        );
+        const zoom = this.olMap!.getView().getZoom()!;
+        const height =
+            this.normalizedImageHeights[transferTemplate.type] *
+            // One higher zoom level means to double the height of the image
+            Math.pow(2, zoom - ImageStyleHelper.normalZoom) *
+            // For some reason we need this additional factor to make it work...
+            2.3;
+        const width = height * (dragImage.width / dragImage.height);
+        // We need a container, because styles on an image element are ignored per API specification (image is interpreted as a bitmap)
+        const container = this.createDragImageContainer(dragImage, height);
+        event.dataTransfer?.setDragImage(container, width / 2, height / 2);
+        // This seems to be optional...
         event.dataTransfer!.dropEffect = 'copy';
         event.dataTransfer!.effectAllowed = 'copy';
+
         event.dataTransfer!.setData(
             this.dragElementKey,
             JSON.stringify(transferTemplate)
         );
+    }
+
+    /**
+     * The container for the drag image has to be somewhere in the DOM,
+     * otherwise the drag image is not rendered
+     * This is the container for this.
+     */
+    private createDragImageContainer(
+        imageElement: HTMLImageElement,
+        height: number
+    ) {
+        const container = document.createElement('div');
+        container.style.height = `${height}px`;
+        // The element must be rendered, but should not be visible (this is a hack -> it could be different from browser to browser)
+        container.style.position = 'absolute';
+        container.style.top = '-1000px';
+        document.body.prepend(container);
+        // We assume that only one drag operation can be made at a time
+        document.addEventListener(
+            'dragend',
+            (event) => {
+                container?.remove();
+            },
+            {
+                once: true,
+            }
+        );
+        container.append(imageElement);
+        // The image should have the size of the container
+        imageElement.style.height = '100%';
+        return container;
     }
 
     public onDrop(event: DragEvent) {
