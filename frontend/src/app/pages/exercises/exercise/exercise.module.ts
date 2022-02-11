ad096e323308f9d7fc6e883b014513a66c65b441
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-02-11 15:14:04 +0100

    Refactor UI and use share API if available

--- frontend/src/app/pages/exercises/exercise/exercise.module.ts
+++ frontend/src/app/pages/exercises/exercise/exercise.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ExerciseComponent } from './exercise/exercise.component';
 import { ClientOverviewModule } from './shared/client-overview/client-overview.module';
 import { ExerciseMapModule } from './shared/exercise-map/exercise-map.module';
@@ -21,7 +20,6 @@ import { TrainerToolbarComponent } from './shared/trainer-toolbar/trainer-toolba
         HttpClientModule,
         ClientOverviewModule,
         ExerciseMapModule,
-        NgbModule,
     ],
     exports: [ExerciseComponent],
 })
