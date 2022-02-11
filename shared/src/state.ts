2a21c572e9cae05675adb975d6cd1f90c7840bad
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-02-11 11:29:32 +0100

    Add participant id to state

--- shared/src/state.ts
+++ shared/src/state.ts
@@ -44,4 +44,5 @@ class ExerciseStateClass {
     public imageTemplates: ImageTemplate[] = [];
     public ecoLog: EocLogEntry[] = [];
     public statusHistory: StatusHistoryEntry[] = [];
+    public participantId = '';
 }
