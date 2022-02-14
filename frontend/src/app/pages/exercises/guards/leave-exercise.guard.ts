3a149037cf9eb880823330dd02e9e0421ba7bbcf
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-02-14 23:34:30 +0100

    Display messages #180

--- frontend/src/app/pages/exercises/guards/leave-exercise.guard.ts
+++ frontend/src/app/pages/exercises/guards/leave-exercise.guard.ts
@@ -5,12 +5,16 @@ import type {
     RouterStateSnapshot,
 } from '@angular/router';
 import { ApiService } from 'src/app/core/api.service';
+import { MessageService } from 'src/app/core/messages/message.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class LeaveExerciseGuard implements CanDeactivate<unknown> {
-    constructor(private readonly apiService: ApiService) {}
+    constructor(
+        private readonly apiService: ApiService,
+        private readonly messageService: MessageService
+    ) {}
 
     canDeactivate(
         component: unknown,
@@ -18,6 +22,11 @@ export class LeaveExerciseGuard implements CanDeactivate<unknown> {
         currentState: RouterStateSnapshot,
         nextState?: RouterStateSnapshot
     ) {
+        this.messageService.postMessage({
+            title: 'Übung verlassen',
+            body: 'Sie können der Übung über die Übungs-ID wieder beitreten.',
+            color: 'info',
+        });
         this.apiService.leaveExercise();
         return true;
     }
