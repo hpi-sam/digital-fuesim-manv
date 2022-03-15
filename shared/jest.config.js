5271e7d8a013f2f69cfdfc808a4a5705e8a78416
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-03-15 15:19:38 +0100

    Fix esm for shared jest

--- shared/jest.config.js
+++ shared/jest.config.js
@@ -5,4 +5,15 @@ export default {
     name: 'shared',
     displayName: 'Shared',
     roots: ['<rootDir>'],
+    // See https://kulshekhar.github.io/ts-jest/docs/next/guides/esm-support/
+    // and https://jestjs.io/docs/ecmascript-modules
+    extensionsToTreatAsEsm: ['.ts'],
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
 };
