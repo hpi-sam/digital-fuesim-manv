187428fb9aae2e3b2a6513c90859aafb42cd67dd
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-04-09 15:43:34 +0200

    Add tests for ExerciseWrapper

--- backend/src/exercise/exercise-wrapper.spec.ts
+++ backend/src/exercise/exercise-wrapper.spec.ts
@@ -3,7 +3,7 @@ import { createTestEnvironment, sleep } from '../../test/utils';
 import { clientMap } from './client-map';
 import { ExerciseWrapper } from './exercise-wrapper';
 
-describe('exercise wrapper', () => {
+describe('Exercise Wrapper', () => {
     const environment = createTestEnvironment();
     it('fails getting a role for the wrong id', () => {
         const exercise = new ExerciseWrapper('participant', 'trainer');
@@ -21,13 +21,13 @@ describe('exercise wrapper', () => {
             await sleep(100);
             const client = clientMap.values().next().value;
 
-            const reduceSpy = jest.spyOn(
+            const applySpy = jest.spyOn(
                 ExerciseWrapper.prototype,
                 'applyAction'
             );
             exercise.addClient(client);
 
-            expect(reduceSpy).not.toHaveBeenCalled();
+            expect(applySpy).not.toHaveBeenCalled();
         });
     });
 
@@ -37,14 +37,61 @@ describe('exercise wrapper', () => {
         await environment.withWebsocket(async () => {
             const client = clientMap.values().next().value;
 
-            const reduceSpy = jest.spyOn(
+            const applySpy = jest.spyOn(
                 ExerciseWrapper.prototype,
                 'applyAction'
             );
-            reduceSpy.mockClear();
+            applySpy.mockClear();
             exercise.removeClient(client);
 
-            expect(reduceSpy).not.toHaveBeenCalled();
+            expect(applySpy).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('Started Exercise', () => {
+        let exercise: ExerciseWrapper | undefined;
+        beforeEach(() => {
+            exercise = new ExerciseWrapper('participant', 'trainer');
+            exercise.start();
+        });
+        afterEach(() => {
+            exercise?.pause();
+            exercise = undefined;
+        });
+        it('emits tick event in tick', async () => {
+            const applySpy = jest.spyOn(
+                ExerciseWrapper.prototype,
+                'applyAction'
+            );
+            const tickInterval = (exercise as any).tickInterval;
+
+            applySpy.mockClear();
+            await sleep(tickInterval);
+            expect(applySpy).toHaveBeenCalledTimes(1);
+            const action = applySpy.mock.calls[0][0];
+            expect(action.type).toBe('[Exercise] Tick');
+        });
+    });
+
+    describe('Reactions to Actions', () => {
+        it('calls start when matching action is sent', () => {
+            const exercise = new ExerciseWrapper('participant', 'trainer');
+
+            const startMock = jest.spyOn(ExerciseWrapper.prototype, 'start');
+            startMock.mockImplementation(() => ({}));
+
+            exercise.applyAction({ type: '[Exercise] Start', timestamp: 0 });
+            expect(startMock).toHaveBeenCalledTimes(1);
+        });
+
+        it('calls start when matching action is sent', () => {
+            const exercise = new ExerciseWrapper('participant', 'trainer');
+
+            const pause = jest.spyOn(ExerciseWrapper.prototype, 'pause');
+            pause.mockImplementation(() => ({}));
+
+            exercise.applyAction({ type: '[Exercise] Pause', timestamp: 0 });
+            expect(pause).toHaveBeenCalledTimes(1);
         });
     });
 });
