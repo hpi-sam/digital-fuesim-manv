ca95f74e193e504299131e6085a8cc0ecf119ac7
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-04-12 15:03:51 +0200

    Fix return type of `getCreate`

--- shared/src/models/utils/get-create.ts
+++ shared/src/models/utils/get-create.ts
@@ -1,5 +1,30 @@
 import type { Constructor } from '../../utils';
 
+/**
+ * Models must be JSON objects. This means they mustn't have any functions.
+ * The `prototype` of a class contains functions (e.g. `constructor`).
+ * We still need the constructor to create a new instance of the class with a prototype, to be able to use `class-transformer`.
+ * We solve this by using a static factory function `create` in the class.
+ *
+ * @example
+ * ```typescript
+ *   export class Position {
+ *       @IsNumber()
+ *       public a: number;
+ *       /**
+ *        * @_deprecated Use {@link create} instead
+ *        *_/
+ *       constructor(a: number) {
+ *           this.a = a;
+ *       }
+ *       static readonly create = getCreate(this);
+ *   }
+ * ```
+ * @param aClass The class whose static create method is to be returned
+ * @returns a function that creates a new instance of {@link aClass} without a prototype
+ */
 export function getCreate<T extends Constructor>(aClass: T) {
-    return (...args: ConstructorParameters<T>) => ({ ...new aClass(...args) });
+    return (...args: ConstructorParameters<T>): InstanceType<T> => ({
+        ...new aClass(...args),
+    });
 }
