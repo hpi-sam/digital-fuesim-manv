d955e721fb62ab83c667bff652355c937a922f91
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-04-19 20:46:04 +0200

    Add ability to connect and disconnect transferPoints

--- shared/src/store/action-reducers/transfer-point.ts
+++ shared/src/store/action-reducers/transfer-point.ts
@@ -1,12 +1,21 @@
 /* eslint-disable @typescript-eslint/no-throw-literal */
 /* eslint-disable @typescript-eslint/no-namespace */
 import { Type } from 'class-transformer';
-import { IsString, IsUUID, ValidateNested } from 'class-validator';
+import {
+    IsNumber,
+    IsOptional,
+    IsString,
+    IsUUID,
+    ValidateNested,
+} from 'class-validator';
 import { TransferPoint } from '../../models';
 import { Position } from '../../models/utils';
+import type { ExerciseState } from '../../state';
+import type { Mutable } from '../../utils';
 import { uuidValidationOptions, UUID } from '../../utils';
 import type { Action, ActionReducer } from '../action-reducer';
 import { ReducerError } from '../reducer-error';
+import { calculateDistance } from './utils/calculate-distance';
 
 export class AddTransferPointAction implements Action {
     @IsString()
@@ -48,6 +57,32 @@ export class RemoveTransferPointAction implements Action {
 
     @IsUUID(4, uuidValidationOptions)
     public readonly transferPointId!: UUID;
+}
+
+export class ConnectTransferPointsAction implements Action {
+    @IsString()
+    public readonly type = '[TransferPoint] Connect TransferPoints';
+
+    @IsUUID(4, uuidValidationOptions)
+    public readonly transferPointId1!: UUID;
+
+    @IsUUID(4, uuidValidationOptions)
+    public readonly transferPointId2!: UUID;
+
+    @IsOptional()
+    @IsNumber()
+    public readonly duration?: number;
+}
+
+export class DisconnectTransferPointsAction implements Action {
+    @IsString()
+    public readonly type = '[TransferPoint] Disconnect TransferPoints';
+
+    @IsUUID(4, uuidValidationOptions)
+    public readonly transferPointId1!: UUID;
+
+    @IsUUID(4, uuidValidationOptions)
+    public readonly transferPointId2!: UUID;
 }
 
 export namespace TransferPointActionReducers {
@@ -96,6 +131,70 @@ export namespace TransferPointActionReducers {
             rights: 'trainer',
         };
 
+    export const connectTransferPoints: ActionReducer<ConnectTransferPointsAction> =
+        {
+            action: ConnectTransferPointsAction,
+            reducer: (
+                draftState,
+                { transferPointId1, transferPointId2, duration }
+            ) => {
+                // If the transferPoints are already connected, we only update the duration
+                // TODO: We currently only support bidirectional connections between different transfer points.
+                if (transferPointId1 === transferPointId2) {
+                    throw new ReducerError(
+                        `TransferPoint with id ${transferPointId1} cannot connect to itself`
+                    );
+                }
+                const transferPoint1 = getTransferPoint(
+                    draftState,
+                    transferPointId1
+                );
+                const transferPoint2 = getTransferPoint(
+                    draftState,
+                    transferPointId2
+                );
+                const _duration =
+                    duration ??
+                    estimateDuration(
+                        transferPoint1.position,
+                        transferPoint2.position
+                    );
+                transferPoint1.reachableTransferPoints[transferPointId2] = {
+                    duration: _duration,
+                };
+                transferPoint2.reachableTransferPoints[transferPointId1] = {
+                    duration: _duration,
+                };
+                return draftState;
+            },
+            rights: 'trainer',
+        };
+
+    export const disconnectTransferPoints: ActionReducer<DisconnectTransferPointsAction> =
+        {
+            action: DisconnectTransferPointsAction,
+            reducer: (draftState, { transferPointId1, transferPointId2 }) => {
+                // We remove the connection from both directions
+                if (transferPointId1 === transferPointId2) {
+                    throw new ReducerError(
+                        `TransferPoint with id ${transferPointId1} cannot connect to itself`
+                    );
+                }
+                const transferPoint1 = getTransferPoint(
+                    draftState,
+                    transferPointId1
+                );
+                const transferPoint2 = getTransferPoint(
+                    draftState,
+                    transferPointId2
+                );
+                delete transferPoint1.reachableTransferPoints[transferPointId2];
+                delete transferPoint2.reachableTransferPoints[transferPointId1];
+                return draftState;
+            },
+            rights: 'trainer',
+        };
+
     export const removeTransferPoint: ActionReducer<RemoveTransferPointAction> =
         {
             action: RemoveTransferPointAction,
@@ -128,4 +227,40 @@ export namespace TransferPointActionReducers {
             },
             rights: 'trainer',
         };
+}
+
+// Helpers
+
+/**
+ * @returns The transferPoint with the given id
+ * @throws ReducerError if the transferPoint does not exist
+ */
+function getTransferPoint(
+    state: Mutable<ExerciseState>,
+    transferPointId: UUID
+) {
+    const transferPoint = state.transferPoints[transferPointId];
+    if (!transferPoint) {
+        throw new ReducerError(
+            `TransferPoint with id ${transferPointId} does not exist`
+        );
+    }
+    return transferPoint;
+}
+
+/**
+ *
+ * @returns an estimated duration to walk between the the two given positions
+ * The resulting value is a multiple of 0.1 minutes.
+ */
+function estimateDuration(startPosition: Position, targetPosition: Position) {
+    // How long in ms it takes to start + stop moving
+    const overheadSummand = 60 * 1000;
+    // How long in ms it takes to move 1 meter
+    const distanceFactor = 1000 * 1;
+    const multipleOf = 1000 * 60 * 0.1;
+    const estimate =
+        overheadSummand +
+        distanceFactor * calculateDistance(startPosition, targetPosition);
+    return Math.round(estimate / multipleOf) * multipleOf;
 }
