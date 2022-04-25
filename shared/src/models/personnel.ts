b7001e99a271bda11699e239eeb3872851f24c6a
anonym-HPI <68286419+anonym-HPI@users.noreply.github.com>
2022-04-25 18:30:10 +0200

    put personnelTemplateMap over class

--- shared/src/models/personnel.ts
+++ shared/src/models/personnel.ts
@@ -16,6 +16,54 @@ import {
     Transfer,
 } from './utils';
 
+const personnelTemplateMap: {
+    [key in PersonnelType]: {
+        image: ImageProperties;
+        canCaterFor: CanCaterFor;
+    };
+} = {
+    san: {
+        image: {
+            url: '/assets/san-personnel.svg',
+            height: 80,
+            aspectRatio: 1,
+        },
+        canCaterFor: CanCaterFor.create(1, 1, 4, 'or'),
+    },
+    notSan: {
+        image: {
+            url: '/assets/notSan-personnel.svg',
+            height: 80,
+            aspectRatio: 1,
+        },
+        canCaterFor: CanCaterFor.create(1, 1, 4, 'or'),
+    },
+    rettSan: {
+        image: {
+            url: '/assets/rettSan-personnel.svg',
+            height: 80,
+            aspectRatio: 1,
+        },
+        canCaterFor: CanCaterFor.create(1, 1, 4, 'or'),
+    },
+    notarzt: {
+        image: {
+            url: '/assets/notarzt-personnel.svg',
+            height: 80,
+            aspectRatio: 1,
+        },
+        canCaterFor: CanCaterFor.create(1, 1, 4, 'or'),
+    },
+    gf: {
+        image: {
+            url: '/assets/gf-personnel.svg',
+            height: 80,
+            aspectRatio: 1,
+        },
+        canCaterFor: CanCaterFor.create(0, 0, 0, 'or'),
+    },
+};
+
 export class Personnel {
     @IsUUID(4, uuidValidationOptions)
     public readonly id: UUID = uuid();
@@ -83,51 +131,3 @@ export class Personnel {
         );
     }
 }
-
-const personnelTemplateMap: {
-    [key in PersonnelType]: {
-        image: ImageProperties;
-        canCaterFor: CanCaterFor;
-    };
-} = {
-    san: {
-        image: {
-            url: '/assets/san-personnel.svg',
-            height: 80,
-            aspectRatio: 1,
-        },
-        canCaterFor: CanCaterFor.create(1, 1, 4, 'or'),
-    },
-    notSan: {
-        image: {
-            url: '/assets/notSan-personnel.svg',
-            height: 80,
-            aspectRatio: 1,
-        },
-        canCaterFor: CanCaterFor.create(1, 1, 4, 'or'),
-    },
-    rettSan: {
-        image: {
-            url: '/assets/rettSan-personnel.svg',
-            height: 80,
-            aspectRatio: 1,
-        },
-        canCaterFor: CanCaterFor.create(1, 1, 4, 'or'),
-    },
-    notarzt: {
-        image: {
-            url: '/assets/notarzt-personnel.svg',
-            height: 80,
-            aspectRatio: 1,
-        },
-        canCaterFor: CanCaterFor.create(1, 1, 4, 'or'),
-    },
-    gf: {
-        image: {
-            url: '/assets/gf-personnel.svg',
-            height: 80,
-            aspectRatio: 1,
-        },
-        canCaterFor: CanCaterFor.create(0, 0, 0, 'or'),
-    },
-};
