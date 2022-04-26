619c429375f5768e1d1b0f517c0924482e662ff5
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-04-26 19:08:52 +0200

    Add scenario specific patients

--- shared/src/data/default-state/patient-templates.ts
+++ shared/src/data/default-state/patient-templates.ts
@@ -90,12 +90,12 @@ const defaultHealthState: MedicalInformation = {
     walkable: true,
 };
 
-const maleStablePatientTemplate = PatientTemplate.create(
+const stablePatient1Template = PatientTemplate.create(
     'SK III, stabil',
     {
         sex: 'male',
-        externalFeatures: '170cm, Glatze, große Brille',
-        age: 18,
+        externalFeatures: '160cm, kurze, braune Haare',
+        age: 25,
     },
     {
         ...defaultHealthState,
@@ -107,8 +107,189 @@ const maleStablePatientTemplate = PatientTemplate.create(
     stableState.id
 );
 
+const stablePatient2Template = PatientTemplate.create(
+    'SK III, stabil',
+    {
+        sex: 'male',
+        externalFeatures: '175cm, Brille',
+        age: 43,
+    },
+    {
+        ...defaultHealthState,
+        pulse: '83',
+    },
+    true,
+    { [stableState.id]: stableState },
+    defaultMaleImage,
+    healthPointsDefaults.greenMax,
+    stableState.id
+);
+
+const stablePatient3Template = PatientTemplate.create(
+    'SK III, stabil',
+    {
+        sex: 'female',
+        externalFeatures: '150cm, lange, blonde Haare',
+        age: 32,
+    },
+    {
+        ...defaultHealthState,
+        pulse: '75',
+    },
+    true,
+    { [stableState.id]: stableState },
+    defaultMaleImage,
+    healthPointsDefaults.greenMax,
+    stableState.id
+);
+
+const carDriverRedDamage = -healthPointsDefaults.redMax / (8 * 60);
+const carDriverRedState1 = PatientHealthState.create(
+    FunctionParameters.create(
+        carDriverRedDamage,
+        -carDriverRedDamage,
+        -carDriverRedDamage,
+        -carDriverRedDamage
+    ),
+    []
+);
+
+const carDriverRedTemplate = PatientTemplate.create(
+    'SK I, Verschlechterung (< 8min), ex',
+    {
+        sex: 'male',
+        externalFeatures: '170cm, braune Haare, extrem adipös',
+        age: 50,
+    },
+    {
+        consciousness: 'bewusstlos, Massenbewegung auf Schmerz',
+        breathing: 'Atemwegsverlegung',
+        pulse: '85, gut tastbar',
+        skin: 'tief zyanotisch',
+        pain: 'entfällt',
+        pupils: 'rechts weit',
+        psyche: '-',
+        hearing: '-',
+        injuries:
+            'Kopfplatzwunde über dem Ohr; Prellmarke Stirn; Gesicht blutverschmiert',
+        bodyCheck: 'Unterarmfraktur rechts',
+        walkable: false,
+    },
+    true,
+    { [carDriverRedState1.id]: carDriverRedState1 },
+    defaultMaleImage,
+    healthPointsDefaults.redMax,
+    carDriverRedState1.id
+);
+
+const carPassengerYellowDamage =
+    -(healthPointsDefaults.yellowMax - healthPointsDefaults.redMax) / (10 * 60);
+
+const carPassengerYellowState1 = PatientHealthState.create(
+    FunctionParameters.create(
+        carPassengerYellowDamage,
+        -carPassengerYellowDamage,
+        -carPassengerYellowDamage,
+        -carPassengerYellowDamage
+    ),
+    [
+        {
+            matchingHealthStateId: stableState.id,
+            maximumHealth: healthPointsDefaults.redMax,
+        },
+    ]
+);
+
+const carPassengerYellowTemplate = PatientTemplate.create(
+    'SK II, Verschlechterung (< 10 min), ex',
+    {
+        sex: 'male',
+        externalFeatures: '170cm, braune Haare, extrem adipös',
+        age: 50,
+    },
+    {
+        consciousness: 'wach, verwirrt',
+        breathing: 'unauffällig',
+        pulse: '123, Puls flach',
+        skin: 'kaltschweiß',
+        pain: 'starke',
+        pupils: 'isocor',
+        psyche: 'aufgeregt',
+        hearing: 'unauffällig',
+        injuries:
+            'Prellmarke Unterschenkel rechts; Fehlstellung rechtes Sprunggelenk; Wunde Unterarm rechts',
+        bodyCheck: 'Druckschmerz rechte Hüfte; Prellung Unterschenkel links',
+        walkable: false,
+    },
+    true,
+    {
+        [stableState.id]: stableState,
+        [carPassengerYellowState1.id]: carPassengerYellowState1,
+    },
+    defaultMaleImage,
+    healthPointsDefaults.yellowMax,
+    carPassengerYellowState1.id
+);
+
+const femaleHitByCarTemplate = PatientTemplate.create(
+    'SK II, stable',
+    {
+        sex: 'female',
+        externalFeatures: 'graue Haare, Brille, grüne Augen, ca. 1,60 m',
+        age: 75,
+    },
+    {
+        consciousness: 'somolent',
+        breathing: 'flache Atmung',
+        pulse: '132, fadenförmig',
+        skin: 'grau marmoriert',
+        pain: 'stärkste',
+        pupils: 'isocor',
+        psyche: 'teilnahmslos',
+        hearing: 'unauffällig',
+        injuries:
+            'große Weichteilquetschung linker Unterschenkel, nur noch mäßig blutend; aber schon deutlicher Blutverlust',
+        bodyCheck:
+            'Oberarmfraktur rechts; fraglicher Fremdkörper rechte Hand in Wunde; blutende Prellmarke am Hinterkopf',
+        walkable: false,
+    },
+    true,
+    { [stableState.id]: stableState },
+    defaultFemaleImage,
+    healthPointsDefaults.yellowMax,
+    stableState.id
+);
+
+const driverCar2Template = PatientTemplate.create(
+    'SK III, stable, teilnahmslos',
+    {
+        sex: 'male',
+        externalFeatures: 'braune Haare, braune Augen, 1,72 m',
+        age: 35,
+    },
+    {
+        consciousness: 'verwirrt',
+        breathing: 'unauffällig',
+        pulse: '82, gut tastbar',
+        skin: 'unauffällig',
+        pain: 'keine',
+        pupils: 'isocor',
+        psyche: 'teilnahmslos',
+        hearing: 'unauffällig',
+        injuries: 'keine äußeren Verletzungen sichtbar',
+        bodyCheck:
+            'Pat. ist teilnahmslos; keine Kooperation bei der Untersuchung',
+        walkable: true,
+    },
+    true,
+    { [stableState.id]: stableState },
+    defaultMaleImage,
+    healthPointsDefaults.greenMax,
+    stableState.id
+);
+
 const greenGettingWorsePatientTemplate = PatientTemplate.create(
-    'SK III, Verschlechterung (< 10 min), bis SK II',
+    '?SK III, Verschlechterung (< 10 min), bis SK II',
     {
         sex: 'male',
         externalFeatures: '160cm, kurze, braune Haare',
@@ -128,7 +309,7 @@ const greenGettingWorsePatientTemplate = PatientTemplate.create(
 );
 
 const greenGettingWorseAfter5minPatientTemplate = PatientTemplate.create(
-    'SK III, stabil (5 min), Verschlechterung (< 10 min), ex',
+    '?SK III, stabil (5 min), Verschlechterung (< 10 min), ex',
     {
         sex: 'male',
         externalFeatures: '180cm, blonde Haare',
@@ -148,7 +329,7 @@ const greenGettingWorseAfter5minPatientTemplate = PatientTemplate.create(
 );
 
 const yellowGettingWorsePatientTemplate = PatientTemplate.create(
-    'SK II, Verschlechterung (< 7 min), ex',
+    '?SK II, Verschlechterung (< 7 min), ex',
     {
         sex: 'male',
         externalFeatures: 'blaue Augen, weiße Haare, 174cm',
@@ -165,7 +346,7 @@ const yellowGettingWorsePatientTemplate = PatientTemplate.create(
 );
 
 const yellowNeedsNotarztPatientTemplate = PatientTemplate.create(
-    'SK II, Notarzt in < 7 min, ex',
+    '?SK II, Notarzt in < 7 min, ex',
     {
         sex: 'male',
         externalFeatures: 'Glatze, 173cm',
@@ -182,7 +363,7 @@ const yellowNeedsNotarztPatientTemplate = PatientTemplate.create(
 );
 
 const redPatientTemplate = PatientTemplate.create(
-    'SK I, Verschlechterung (< 4 min), ex',
+    '?SK I, Verschlechterung (< 4 min), ex',
     {
         sex: 'female',
         externalFeatures: 'grüne Augen, graue Haare, 167cm',
@@ -226,7 +407,13 @@ const blackPatientTemplate = PatientTemplate.create(
 );
 
 export const defaultPatientTemplates: readonly PatientTemplate[] = [
-    maleStablePatientTemplate,
+    stablePatient1Template,
+    stablePatient2Template,
+    stablePatient3Template,
+    carDriverRedTemplate,
+    carPassengerYellowTemplate,
+    femaleHitByCarTemplate,
+    driverCar2Template,
     greenGettingWorsePatientTemplate,
     greenGettingWorseAfter5minPatientTemplate,
     yellowGettingWorsePatientTemplate,
