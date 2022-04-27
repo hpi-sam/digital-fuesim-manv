a2e43c25fb0b0f251f111a56e8be2da63d0fc18e
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-04-27 09:53:14 +0200

    Update tile server url

--- shared/src/data/default-state/tile-map-properties.ts
+++ shared/src/data/default-state/tile-map-properties.ts
@@ -2,6 +2,6 @@ import type { TileMapProperties } from '../../models';
 
 export const defaultTileMapProperties: TileMapProperties = {
     tileUrl:
-        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoiZGFzc2RlcmRpZSIsImEiOiJja3dzNnZsM3ExM3BzMnBxb3lsZTAwZnN0In0.64CnzzdAgbmKwNkldgS5cg',
     maxZoom: 20,
 };
