9a6b4370f8d8769738d234ee83148cdcfdc16f70
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-05-12 23:35:28 +0200

    Extract thinSpace into a constant

--- frontend/src/app/shared/pipes/format-duration.pipe.ts
+++ frontend/src/app/shared/pipes/format-duration.pipe.ts
@@ -22,23 +22,24 @@ export class FormatDurationPipe implements PipeTransform {
 
         const parts = [];
         if (daysStr > 0) {
-            // \u2009 is a thin space
-            parts.push(`${daysStr}\u2009d`);
+            parts.push(`${daysStr}${thinSpace}d`);
         }
         if (hoursStr > 0) {
-            parts.push(`${hoursStr}\u2009h`);
+            parts.push(`${hoursStr}${thinSpace}h`);
         }
         if (minutesStr > 0) {
-            parts.push(`${minutesStr}\u2009min`);
+            parts.push(`${minutesStr}${thinSpace}min`);
         }
         // we only want to show seconds if there are no other parts because they change too fast
         if (secondsStr > 0 && parts.length === 0) {
-            parts.push(`${secondsStr}\u2009s`);
+            parts.push(`${secondsStr}${thinSpace}s`);
         }
         if (parts.length === 0) {
-            return '0\u2009s';
+            return `0${thinSpace}s`;
         }
 
         return parts.join(' ');
     }
 }
+
+const thinSpace = `\u2009`;
