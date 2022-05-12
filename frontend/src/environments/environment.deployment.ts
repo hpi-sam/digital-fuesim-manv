d6f853dc639723f85d7fbdd5625dac01205ddacd
anonym-HPI <68286419+anonym-HPI@users.noreply.github.com>
2022-05-12 17:19:25 +0200

    get location port from browser in deployment

--- frontend/src/environments/environment.deployment.ts
+++ frontend/src/environments/environment.deployment.ts
@@ -2,6 +2,14 @@ import type { Environment } from './environment-type';
 
 export const environment: Environment = {
     production: true,
-    httpPort: 80,
-    websocketPort: 80,
+    httpPort:
+        Number.parseInt(window.location.port) ||
+        window.location.protocol === 'https:'
+            ? 443
+            : 80,
+    websocketPort:
+        Number.parseInt(window.location.port) ||
+        window.location.protocol === 'https:'
+            ? 443
+            : 80,
 };
