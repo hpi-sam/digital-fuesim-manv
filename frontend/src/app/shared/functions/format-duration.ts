b138507dca036325b1cc7c1d4a0dfabb38dd68da
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-05-20 15:29:41 +0200

    Change formatDuration

--- frontend/src/app/shared/functions/format-duration.ts
+++ frontend/src/app/shared/functions/format-duration.ts
@@ -8,30 +8,18 @@ export function formatDuration(duration: number): string {
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
 
-    const secondsStr = seconds % 60;
-    const minutesStr = minutes % 60;
-    const hoursStr = hours % 24;
-    const daysStr = days;
+    const secondsStr = (seconds % 60).toLocaleString(undefined, {
+        minimumIntegerDigits: 2,
+    });
+    const minutesStr = (minutes % 60).toLocaleString(undefined, {
+        minimumIntegerDigits: 2,
+    });
+    const hoursStr = (hours % 24).toLocaleString(undefined, {
+        minimumIntegerDigits: 2,
+    });
+    const daysStr = days > 0 ? `${days}${thinSpace}d ` : '';
 
-    const parts = [];
-    if (daysStr > 0) {
-        parts.push(`${daysStr}${thinSpace}d`);
-    }
-    if (hoursStr > 0) {
-        parts.push(`${hoursStr}${thinSpace}h`);
-    }
-    if (minutesStr > 0) {
-        parts.push(`${minutesStr}${thinSpace}min`);
-    }
-    // we only want to show seconds if there are no other parts because they change too fast
-    if (secondsStr > 0 && parts.length === 0) {
-        parts.push(`${secondsStr}${thinSpace}s`);
-    }
-    if (parts.length === 0) {
-        return `0${thinSpace}s`;
-    }
-
-    return parts.join(' ');
+    return `${daysStr}${hoursStr}:${minutesStr}:${secondsStr}`;
 }
 
 const thinSpace = `\u2009`;
