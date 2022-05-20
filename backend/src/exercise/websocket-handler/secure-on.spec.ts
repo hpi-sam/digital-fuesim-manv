afac23d1fe4990d003a092aa2093da822c6cc78c
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-05-21 00:22:52 +0200

    Apply feedback

--- backend/src/exercise/websocket-handler/secure-on.spec.ts
+++ backend/src/exercise/websocket-handler/secure-on.spec.ts
@@ -4,40 +4,19 @@ describe('secure on', () => {
     const environment = createTestEnvironment();
     it('does not crash in production environment when fed with invalid data', async () => {
         const originalEnv = process.env.NODE_ENV;
+        // Errors are only catched in production mode
         process.env.NODE_ENV = 'production';
-        await environment.withWebsocket(async (socket) => {
-            const promise = new Promise<void>((resolve) => {
+        try {
+            await environment.withWebsocket(async (socket) => {
                 socket.insecureEmit('getState', 'any');
                 // wait a bit so that the error can occur
-                sleep(1000).then(async () => {
-                    // Check whether the server is still available
-                    await environment
-                        .httpRequest('get', '/api/health')
-                        .expect(200);
-                    resolve();
-                });
+                await sleep(1000);
+                // Check whether the server is still available
+                await environment.httpRequest('get', '/api/health').expect(200);
             });
-            await promise;
-        });
-        process.env.NODE_ENV = originalEnv;
+        } finally {
+            process.env.NODE_ENV = originalEnv;
+        }
     });
-    // it('does crash in development environment when fed with invalid data', async () => {
-    //     const originalEnv = process.env.NODE_ENV;
-    //     process.env.NODE_ENV = 'development';
-    //     await environment.withWebsocket(async (socket) => {
-    //         const promise = new Promise<void>((resolve) => {
-    //             socket.insecureEmit('getState', 'any');
-    //             // wait a bit so that the error can occur
-    //             sleep(1000).then(async () => {
-    //                 // Check whether the server is still available
-    //                 await environment
-    //                     .httpRequest('get', '/api/health')
-    //                     .expect(200);
-    //                 resolve();
-    //             });
-    //         });
-    //         await promise;
-    //     });
-    //     process.env.NODE_ENV = originalEnv;
-    // });
+    // TODO: test that it does crash in development environment when fed with invalid data
 });
