cc236e31802c5572358606ef0dc354b9c6d547de
Clemens <68013019+ClFeSc@users.noreply.github.com>
2022-05-21 00:38:28 +0200

    Update backend/src/exercise/websocket-handler/secure-on.spec.ts
    Co-authored-by: Julian Schmidt <julian.schmidt@student.hpi.de>

--- backend/src/exercise/websocket-handler/secure-on.spec.ts
+++ backend/src/exercise/websocket-handler/secure-on.spec.ts
@@ -6,17 +6,17 @@ describe('secure on', () => {
         const originalEnv = process.env.NODE_ENV;
         // Errors are only catched in production mode
         process.env.NODE_ENV = 'production';
-        try {
-            await environment.withWebsocket(async (socket) => {
+        await environment
+            .withWebsocket(async (socket) => {
                 socket.insecureEmit('getState', 'any');
                 // wait a bit so that the error can occur
                 await sleep(1000);
                 // Check whether the server is still available
                 await environment.httpRequest('get', '/api/health').expect(200);
+            })
+            .finally(() => {
+                process.env.NODE_ENV = originalEnv;
             });
-        } finally {
-            process.env.NODE_ENV = originalEnv;
-        }
     });
     // TODO: test that it does crash in development environment when fed with invalid data
 });
