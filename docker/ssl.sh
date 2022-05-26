829b4b2c3deaafa0bb220836da1ae82903a5ca8c
anonym-HPI <68286419+anonym-HPI@users.noreply.github.com>
2022-05-27 00:50:45 +0200

    removing dhparam.pem as we use ECDHE with the chosen certs

--- docker/ssl.sh
+++ docker/ssl.sh
@@ -14,7 +14,7 @@ CERTS_PATH="/ssl/certs"
 mkdir -p /var/www/acme-challenge/.well-known/acme-challenge
 chown -R www-data:www-data /var/www/acme-challenge
 
-# ignore "that no renewal is needed" and for the dhparam check command
+# ignore "that no renewal is needed"
 set +e
 
  acme.sh --issue \
@@ -30,17 +30,11 @@ set +e
 --server letsencrypt \
 --reloadcmd "nginx -s reload"
 
-if openssl dhparam -check < ${CERTS_PATH}/dhparam.pem > /dev/null; then
-    echo "Skip: ${CERTS_PATH}/dhparam.pem exists already, not creating new one"
-else
-    openssl dhparam -out ${CERTS_PATH}/dhparam.pem 4096
-fi
-
 set -e
 
 # TODO: checking if anything valid is in the files, e.g. files could be empty
-if [[ -f "${CERTS_PATH}/dhparam.pem"  && -f "${CERTS_PATH}/cert.pem" && -f "${CERTS_PATH}/key.pem" && -f "${CERTS_PATH}/fullchain.pem" ]]; then
-    echo "Sucess: all needed cert files and dhparam.pem exist"
+if [[ -f "${CERTS_PATH}/cert.pem" && -f "${CERTS_PATH}/key.pem" && -f "${CERTS_PATH}/fullchain.pem" ]]; then
+    echo "Sucess: all needed cert files exist"
 else
     echo "Erro: Not all necessary certs exits in ${CERTS_PATH}, maybe acme.sh couldn't connect to letsencrypt or letsencrypt couldn't reach this server over http port 80"
     exit 1
