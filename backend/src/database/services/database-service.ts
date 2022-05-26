808bd10fe473bade6a5eb9bf4af72e11ac19975c
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-05-26 23:16:24 +0200

    Prevent nested transactions through `DatabaseService`

--- backend/src/database/services/database-service.ts
+++ backend/src/database/services/database-service.ts
@@ -1,4 +1,5 @@
 import type { DataSource, EntityManager } from 'typeorm';
+import { DatabaseError } from '../database-error';
 import { ActionWrapperService } from './action-wrapper.service';
 import { ExerciseWrapperService } from './exercise-wrapper.service';
 
@@ -6,7 +7,7 @@ export class DatabaseService {
     public readonly actionWrapperService: ActionWrapperService;
     public readonly exerciseWrapperService: ExerciseWrapperService;
 
-    public constructor(public readonly dataSource: DataSource) {
+    public constructor(private readonly dataSource: DataSource) {
         this.exerciseWrapperService = new ExerciseWrapperService(dataSource);
         this.actionWrapperService = new ActionWrapperService(
             this.exerciseWrapperService,
@@ -18,12 +19,22 @@ export class DatabaseService {
         return this.dataSource.isInitialized;
     }
 
+    private transactionActive = false;
+
     /**
      * Wrap all operations into one database transaction. Note that all calls inside MUST use the provided manager.
      */
     public async transaction<T>(
         operation: (manager: EntityManager) => Promise<T>
     ) {
-        return this.dataSource.transaction(operation);
+        if (this.transactionActive) {
+            throw new DatabaseError(
+                'A transaction is already running; nested transactions are not supported.'
+            );
+        }
+        this.transactionActive = true;
+        const transactionResult = await this.dataSource.transaction(operation);
+        this.transactionActive = false;
+        return transactionResult;
     }
 }
