9d6f30f04370627ab35b964efa82736588eb5d16
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-06-08 11:40:11 +0200

    Add secure http handler

---
+++ backend/src/exercise/http-handler/secure-http.ts
@@ -0,0 +1,54 @@
+// TODO: How bad is that?
+// eslint-disable-next-line @typescript-eslint/no-shadow
+import type { Express, Request } from 'express';
+import type { HttpResponse } from './utils';
+
+export type HttpMethod =
+    | 'delete'
+    | 'get'
+    | 'head'
+    | 'options'
+    | 'patch'
+    | 'post'
+    | 'put';
+
+export async function secureHttp<
+    Method extends HttpMethod,
+    Result extends object | undefined
+>(
+    app: Express,
+    method: Method,
+    route: string,
+    operation: (
+        req: Request
+    ) => HttpResponse<Result> | Promise<HttpResponse<Result>>
+) {
+    // TODO: Better type `req` to verify the arguments
+    app[method](route, async (req, res) => {
+        let response: HttpResponse<Result>;
+        try {
+            response = await operation(req);
+        } catch (error: unknown) {
+            // Try sending 500 response
+            try {
+                const message = `An error occurred on http request: ${error}`;
+                console.warn(message);
+                res.send({
+                    statusCode: 500,
+                    body: { message },
+                });
+            } catch (innerError: unknown) {
+                // Nothing works. Log if in production mode, otherwise re-throw inner error
+                if (process.env.NODE_ENV !== 'production') {
+                    throw innerError;
+                }
+                console.warn(
+                    `An error occurred while handling above http error and trying to respond to client: ${innerError}`
+                );
+            }
+            return;
+        }
+        res.statusCode = response.statusCode;
+        res.send(response.body);
+    });
+}
--- backend/src/exercise/http-server.ts
+++ backend/src/exercise/http-server.ts
@@ -1,6 +1,8 @@
 import type { Server as HttpServer } from 'node:http';
 import cors from 'cors';
-import type * as core from 'express-serve-static-core';
+// TODO: How bad is that?
+// eslint-disable-next-line @typescript-eslint/no-shadow
+import type { Express, Request } from 'express';
 import express from 'express';
 import type { DatabaseService } from '../database/services/database-service';
 import {
@@ -10,14 +12,11 @@ import {
     postExercise,
 } from './http-handler/api/exercise';
 import { getHealth } from './http-handler/api/health';
+import { secureHttp } from './http-handler/secure-http';
 
 export class ExerciseHttpServer {
     public readonly httpServer: HttpServer;
-    constructor(
-        app: core.Express,
-        port: number,
-        databaseService: DatabaseService
-    ) {
+    constructor(app: Express, port: number, databaseService: DatabaseService) {
         // TODO: Temporary allow all
         app.use(cors());
 
@@ -26,35 +25,29 @@ export class ExerciseHttpServer {
         // This endpoint is used to determine whether the API itself is running.
         // It should be independent from any other services that may or may not be running.
         // This is used for the Cypress CI.
-        app.get('/api/health', (req, res) => {
-            const response = getHealth();
-            res.statusCode = response.statusCode;
-            res.send(response.body);
-        });
+        secureHttp(app, 'get', '/api/health', () => getHealth());
 
-        app.post('/api/exercise', async (req, res) => {
-            const response = await postExercise(databaseService, req.body);
-            res.statusCode = response.statusCode;
-            res.send(response.body);
-        });
+        secureHttp(app, 'post', '/api/exercise', async (req: Request) =>
+            postExercise(databaseService, req.body)
+        );
 
-        app.get('/api/exercise/:exerciseId', (req, res) => {
-            const response = getExercise(req.params.exerciseId);
-            res.statusCode = response.statusCode;
-            res.send(response.body);
-        });
+        secureHttp(app, 'get', '/api/exercise/:exerciseId', (req: Request) =>
+            getExercise(req.params.exerciseId)
+        );
 
-        app.delete('/api/exercise/:exerciseId', async (req, res) => {
-            const response = await deleteExercise(req.params.exerciseId);
-            res.statusCode = response.statusCode;
-            res.send(response.body);
-        });
+        secureHttp(
+            app,
+            'delete',
+            '/api/exercise/:exerciseId',
+            async (req: Request) => deleteExercise(req.params.exerciseId)
+        );
 
-        app.get('/api/exercise/:exerciseId/history', async (req, res) => {
-            const response = await getExerciseHistory(req.params.exerciseId);
-            res.statusCode = response.statusCode;
-            res.send(response.body);
-        });
+        secureHttp(
+            app,
+            'get',
+            '/api/exercise/:exerciseId/history',
+            async (req: Request) => getExerciseHistory(req.params.exerciseId)
+        );
 
         this.httpServer = app.listen(port);
     }
