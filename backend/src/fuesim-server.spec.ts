84f263f1c08b56778e3fa5a1976ee84bdc6a4499
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-07-20 18:11:59 +0200

    Add regression test for #455

---
+++ backend/src/fuesim-server.spec.ts
@@ -0,0 +1,59 @@
+import { jest } from '@jest/globals';
+import { Config } from 'config';
+import { uuid } from 'digital-fuesim-manv-shared';
+import { exerciseMap } from 'exercise/exercise-map';
+import { EntityManager } from 'typeorm';
+import { createExercise, createTestEnvironment } from '../test/utils';
+
+describe('Exercise saving', () => {
+    const environment = createTestEnvironment();
+
+    // TODO: This test heavily relies on implementation detail.
+    // When refactoring any part of the application that may impact this test and make it fail, it's ok to change it.
+    // The problem that should be catched by it:
+    // The `FuesimServer` saves all actions every ten seconds.
+    // After saving, the actions are removed from memory.
+    // As the saving happens asynchronously, it's possible that an action gets added between collecting the actions to be saved and removing the actions.
+    // In a naive implementation it can happen that such actions get removed from memory without being saved to the database.
+    it('does not throw away actions while saving', async () => {
+        // Only run this test when a database gets used
+        expect(Config.useDb).toBe(true);
+
+        const exercideIds = await createExercise(environment);
+        const exercise = exerciseMap.get(exercideIds.trainerId)!;
+
+        const saveTick: () => Promise<void> = (environment.server as any)
+            .saveTick;
+
+        const savedEntities: unknown[] = [];
+
+        const saveMock = jest
+            .spyOn(EntityManager.prototype, 'save')
+            .mockImplementation((async (entities: unknown[]) => {
+                // Simulate adding an action while asynchronously waiting
+                exercise.applyAction(
+                    {
+                        type: '[AlarmGroup] Add AlarmGroup',
+                        alarmGroup: {
+                            alarmGroupVehicles: {},
+                            id: uuid(),
+                            name: 'Alarm Group',
+                        },
+                    },
+                    null
+                );
+                savedEntities.push(...entities);
+                return entities;
+                // TODO: Better type
+            }) as any);
+
+        await saveTick();
+
+        expect(saveMock).toHaveBeenCalledTimes(2);
+
+        // One saved exercise, one saved action (set participant id), two actions added while saving
+        expect(
+            savedEntities.length + exercise.temporaryActionHistory.length
+        ).toBe(4);
+    });
+});
