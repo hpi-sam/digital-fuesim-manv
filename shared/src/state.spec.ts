f185788d22af5ca75e3ea6c52ad44c93f2e63ea2
Clemens <68013019+ClFeSc@users.noreply.github.com>
2022-07-25 00:12:34 +0200

    Update shared/src/state.spec.ts
    Co-authored-by: Julian Schmidt <julian.schmidt@student.hpi.de>

--- shared/src/state.spec.ts
+++ shared/src/state.spec.ts
@@ -2,7 +2,8 @@ import { ExerciseState } from './state';
 import { validateExerciseState } from './store';
 
 describe('ExerciseState', () => {
-    it('validates a fresh state', () => {
+    // If this fails, either the created state is invalid, or a validator is incorrect/missing in a model.
+    it('creates a valid state', () => {
         const exercise = ExerciseState.create();
         const validation = validateExerciseState(exercise);
         expect(validation.length).toBe(0);
