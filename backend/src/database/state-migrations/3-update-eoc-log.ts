4d1450aa366817843ee8b4d78a635c1947990a2f
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-07-24 12:51:13 +0200

    Fix eoc log to current design
    * Rename `ecoLog` to `eocLog` (#391)
    * Use `number` instead of `Date` for `exerciseTimestamp`
    * Store the name of the client that wrote the entry instead of its id.
    This allows to still see the name after a disconnect.
    * Restrict length of `message` to `65_535` (removing any longer parts in
    the state migration)
    * This bumps the state version from 2 to 3

---
+++ backend/src/database/state-migrations/3-update-eoc-log.ts
@@ -0,0 +1,58 @@
+import type {
+    Client,
+    EocLogEntry,
+    ImmutableDate,
+    UUID,
+} from 'digital-fuesim-manv-shared';
+import type { Migration } from './migrations';
+
+export const updateEocLog3: Migration = {
+    actions: null,
+    state: migrateState,
+};
+
+function migrateState(state: object): object {
+    // Rename `ecoLog` to `eocLog`
+    (state as { eocLog: EocLogEntry[] }).eocLog = (
+        state as { ecoLog: EocLogEntry[] }
+    ).ecoLog;
+
+    delete (state as { ecoLog?: EocLogEntry[] }).ecoLog;
+
+    (state as { eocLog: EocLogEntry[] }).eocLog.forEach((entry) =>
+        convertEocLogEntryInPlace(
+            entry,
+            (state as { clients: { [key: UUID]: Client } }).clients
+        )
+    );
+    return state;
+}
+
+/**
+ *
+ * @param entry The {@link EocLogEntry} for version 2
+ */
+function convertEocLogEntryInPlace(
+    entry: object,
+    clients: { [key: UUID]: Client }
+): EocLogEntry {
+    interface EocLogEntry3 {
+        id: UUID;
+        timestamp: ImmutableDate;
+        exerciseTimestamp: number;
+        message: string;
+        clientName: string;
+    }
+    (entry as EocLogEntry3).exerciseTimestamp = Math.floor(
+        (entry as { exerciseTimestamp: Date }).exerciseTimestamp.getTime() /
+            1000
+    );
+    (entry as EocLogEntry3).message = (entry as EocLogEntry3).message.slice(
+        0,
+        65535
+    );
+    (entry as EocLogEntry3).clientName =
+        clients[(entry as { clientId: UUID }).clientId]?.name ??
+        'Unbekannter Autor';
+    return entry as EocLogEntry;
+}
