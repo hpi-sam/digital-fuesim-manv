8a34528180dfcd0414a38daa5cea912808e5f8cd
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-07-24 19:35:16 +0200

    Add basic UI for eoc log

---
+++ frontend/src/app/pages/exercises/exercise/shared/emergency-operations-center/eoc-log-interface/eoc-log-interface.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ApiService } from 'src/app/core/api.service';
+import { MessageService } from 'src/app/core/messages/message.service';
+import type { AppState } from 'src/app/state/app.state';
+import { selectEocLogEntries } from 'src/app/state/exercise/exercise.selectors';
+import { getStateSnapshot } from 'src/app/state/get-state-snapshot';
+
+@Component({
+    selector: 'app-eoc-log-interface',
+    templateUrl: './eoc-log-interface.component.html',
+    styleUrls: ['./eoc-log-interface.component.scss'],
+})
+export class EocLogInterfaceComponent {
+    public readonly eocLogEntries$ = this.store.select(selectEocLogEntries);
+
+    public message = '';
+
+    constructor(
+        private readonly apiService: ApiService,
+        private readonly store: Store<AppState>,
+        private readonly messageService: MessageService
+    ) {}
+
+    public async addEocLogEntry() {
+        const clientId = this.apiService.ownClientId;
+        if (!clientId) {
+            this.messageService.postMessage({
+                title: 'Kann keinen Log-Eintrag erstellen!',
+                body:
+                    clientId === null
+                        ? 'Zeitreise aktiviert'
+                        : 'Keiner Übung beigetreten',
+                color: 'danger',
+            });
+            return;
+        }
+        const response = await this.apiService.proposeAction({
+            type: '[Emergency Operation Center] Add Log Entry',
+            message: this.message,
+            name: getStateSnapshot(this.store).exercise.clients[clientId]!.name,
+            timestamp: Date.now(),
+        });
+        if (response.success) {
+            this.messageService.postMessage({
+                title: 'Log-Eintrag hinzugefügt!',
+                color: 'success',
+            });
+            this.message = '';
+        }
+    }
+}
