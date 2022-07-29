0e9ec407b14f1c9b799f60718d0d78d8fffab793
Clemens <68013019+ClFeSc@users.noreply.github.com>
2022-07-29 16:24:19 +0200

    Update backend/src/utils/array.ts
    Improve comment for `pushAll`
    
    Co-authored-by: Julian Schmidt <julian.schmidt@student.hpi.de>

--- backend/src/utils/array.ts
+++ backend/src/utils/array.ts
@@ -3,7 +3,7 @@
  * @param array The array to append to
  * @param elements The elements to append
  *
- * @remark Use this where something like `.push(...Array(300000).fill(1))` is possible.
+ * @remark You can't use `myArray.push(...elementsToBeAdded)` if `elementsToBeAdded` could have more than ca. 130,000 elements, because there is a maximum number of function parameters (based in the stack size). This function works with such big arrays too.
  */
 export function pushAll<T>(array: T[], elements: T[]): void {
     elements.forEach((element) => array.push(element));
