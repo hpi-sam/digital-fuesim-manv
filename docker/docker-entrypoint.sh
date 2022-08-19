b415fbf495263ec9e2a487b883fd36f8349702b3
anonym-HPI <68286419+anonym-HPI@users.noreply.github.com>
2022-08-19 19:22:34 +0200

    fix migration running even when DFM_USE_DB is false

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -43,6 +43,8 @@ if ${DFM_ENABLE_SSL}; then
 
 fi
 
-NODE_ENV=migration node --experimental-specifier-resolution=node ./node_modules/typeorm/cli -d dist/src/database/migration-datasource.js migration:run
+if ${DFM_USE_DB}; then
+    NODE_ENV=migration node --experimental-specifier-resolution=node ./node_modules/typeorm/cli -d dist/src/database/migration-datasource.js migration:run
+fi
 
 NODE_ENV=production node --experimental-specifier-resolution=node dist/src/index.js
