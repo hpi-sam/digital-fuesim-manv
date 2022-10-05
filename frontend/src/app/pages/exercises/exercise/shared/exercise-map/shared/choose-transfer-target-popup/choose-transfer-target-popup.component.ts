71936853e427d49a74158908b3060ef5f43bd795
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2022-10-05 14:10:27 +0200

    Fix incorrect droppedElementType

--- frontend/src/app/pages/exercises/exercise/shared/exercise-map/shared/choose-transfer-target-popup/choose-transfer-target-popup.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/exercise-map/shared/choose-transfer-target-popup/choose-transfer-target-popup.component.ts
@@ -20,7 +20,7 @@ export class ChooseTransferTargetPopupComponent
 {
     // These properties are only set after OnInit
     public transferPointId!: UUID;
-    public droppedElementType!: 'personnel' | 'vehicle';
+    public droppedElementType!: 'personnel' | 'vehicles';
 
     public transferToCallback!: (
         targetId: UUID,
