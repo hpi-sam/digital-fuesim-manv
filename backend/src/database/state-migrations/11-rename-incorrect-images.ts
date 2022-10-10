3ebcdace055740e7c672f9dcdc7fcb5a468982f1
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2022-10-10 19:16:01 +0200

    Fix incorrect image url for patient templates

---
+++ backend/src/database/state-migrations/11-rename-incorrect-images.ts
@@ -0,0 +1,20 @@
+import type { Migration } from './migrations';
+
+export const renameIncorrectImages11: Migration = {
+    actions: null,
+    state: (state) => {
+        (
+            state as {
+                patientCategories: {
+                    patientTemplates: { image: { url: string } }[];
+                }[];
+            }
+        ).patientCategories.forEach((patientCategory) =>
+            patientCategory.patientTemplates.forEach((patientTemplate) => {
+                if (patientTemplate.image.url === '/assets/male-patient.svg') {
+                    patientTemplate.image.url = '/assets/patient.svg';
+                }
+            })
+        );
+    },
+};
