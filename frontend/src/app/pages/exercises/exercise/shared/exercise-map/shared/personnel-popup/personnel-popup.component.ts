c742a6d7e25dcedffbbf50cd1a199921a7625c6d
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2022-10-12 14:05:38 +0200

    Visualize the treatment capacity for personnel and material (#543)
    Co-authored-by: Benildur Nickel <82985280+benn02@users.noreply.github.com>
    
    WIP
    
    Finish cater capacity component
    
    Remove some unneeded <div>s and <span>s in popups
    
    Update header of personnel and material popups
    
    Update wording on personnel and material popups
    
    Update wording again

---
+++ frontend/src/app/pages/exercises/exercise/shared/exercise-map/shared/personnel-popup/personnel-popup.component.ts
@@ -0,0 +1,29 @@
+import type { OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import type { UUID, Personnel } from 'digital-fuesim-manv-shared';
+import type { Observable } from 'rxjs';
+import type { AppState } from 'src/app/state/app.state';
+import { getSelectPersonnel } from 'src/app/state/exercise/exercise.selectors';
+import type { PopupComponent } from '../../utility/popup-manager';
+
+@Component({
+    selector: 'app-personnel-popup',
+    templateUrl: './personnel-popup.component.html',
+    styleUrls: ['./personnel-popup.component.scss'],
+})
+export class PersonnelPopupComponent implements PopupComponent, OnInit {
+    public personnelId!: UUID;
+
+    @Output() readonly closePopup = new EventEmitter<void>();
+
+    public personnel$?: Observable<Personnel>;
+
+    constructor(private readonly store: Store<AppState>) {}
+
+    ngOnInit(): void {
+        this.personnel$ = this.store.select(
+            getSelectPersonnel(this.personnelId)
+        );
+    }
+}
