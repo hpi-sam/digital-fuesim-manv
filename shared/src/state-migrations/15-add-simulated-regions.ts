e435c6538c128307eb4d5c740d394eab1c9d8a4d
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2023-01-17 15:17:15 +0100

    Fix linter error

--- shared/src/state-migrations/15-add-simulated-regions.ts
+++ shared/src/state-migrations/15-add-simulated-regions.ts
@@ -1,8 +1,8 @@
-import type { Migration } from "./migration-functions";
+import type { Migration } from './migration-functions';
 
 export const addSimulatedRegions15: Migration = {
     actions: null,
     state: (state: any) => {
         state.simulatedRegions = {};
-    }
-}
+    },
+};
