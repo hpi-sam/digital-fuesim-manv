b480fcbd223690883d514321373b6b88d52b9b1b
benn02 <82985280+benn02@users.noreply.github.com>
2023-01-24 01:06:02 +0100

    Update shared/src/models/patient.ts
    Co-authored-by: Julian Schmidt <julian.schmidt@student.hpi.de>

--- shared/src/models/patient.ts
+++ shared/src/models/patient.ts
@@ -202,7 +202,6 @@ export class Patient {
 
     static isInVehicle(patient: Patient): boolean {
         return (
-            patient.metaPosition !== undefined &&
             patient.metaPosition.type === 'vehicle'
         );
     }
