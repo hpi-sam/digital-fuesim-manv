e05931881e023a5da9473740184246f48a39ad77
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2023-01-31 15:31:06 +0100

    Remove generic type LineString | Point in favor of the default Geometry (#634)
    * Remove generic type LineString | Point in favor of the default Geometry
    
    * Fix linter

--- frontend/src/app/pages/exercises/exercise/shared/exercise-map/utility/ol-map-manager.ts
+++ frontend/src/app/pages/exercises/exercise/shared/exercise-map/utility/ol-map-manager.ts
@@ -423,15 +423,8 @@ export class OlMapManager {
                         return false;
                     }
                     this.layerFeatureManagerDictionary
-                        .get(
-                            layer as VectorLayer<
-                                VectorSource<LineString | Point>
-                            >
-                        )!
-                        .onFeatureClicked(
-                            event,
-                            feature as Feature<LineString | Point>
-                        );
+                        .get(layer as VectorLayer<VectorSource>)!
+                        .onFeatureClicked(event, feature as Feature);
                     // we only want the top one -> a truthy return breaks this loop
                     return true;
                 },
@@ -473,15 +466,11 @@ export class OlMapManager {
                 }
                 // We stop propagating the event as soon as the onFeatureDropped function returns true
                 return this.layerFeatureManagerDictionary
-                    .get(
-                        layer as VectorLayer<VectorSource<LineString | Point>>
-                    )!
+                    .get(layer as VectorLayer<VectorSource>)!
                     .onFeatureDrop(
                         event,
-                        event.features.getArray()[0] as Feature<
-                            LineString | Point
-                        >,
-                        feature as Feature<Point>
+                        event.features.getArray()[0]!,
+                        feature as Feature
                     );
             });
         });
