88d0fa7ae6c2b99b62cf950b41c3f7dccb3af22d
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-02-02 17:09:20 +0100

    Make content observable

--- frontend/src/app/pages/about/template/template.component.ts
+++ frontend/src/app/pages/about/template/template.component.ts
@@ -1,17 +1,16 @@
 import { Location as NgLocation } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import type { OnDestroy, OnInit } from '@angular/core';
+import type { OnInit } from '@angular/core';
 import { Component, Input } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+import type { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-template',
     templateUrl: './template.component.html',
     styleUrls: ['./template.component.scss'],
 })
-export class TemplateComponent implements OnDestroy, OnInit {
-    private readonly destroy$ = new Subject<void>();
-    content?: string;
+export class TemplateComponent implements OnInit {
+    content$!: Observable<string>;
 
     /**
      * The title of the page shown as h2 headline.
@@ -29,14 +28,9 @@ export class TemplateComponent implements OnDestroy, OnInit {
     ) {}
 
     ngOnInit(): void {
-        this.http
-            .get(`assets/about/${this.contentFile}`, { responseType: 'text' })
-            .pipe(takeUntil(this.destroy$))
-            .subscribe((response) => (this.content = response));
-    }
-
-    ngOnDestroy(): void {
-        this.destroy$.next();
+        this.content$ = this.http.get(`assets/about/${this.contentFile}`, {
+            responseType: 'text',
+        });
     }
 
     back(event: MouseEvent): void {
