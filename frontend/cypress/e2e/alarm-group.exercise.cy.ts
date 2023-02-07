6909f5d7ab3ccfc0337fa0b321b886b65c2ec0e0
Greenscreen23 <43916057+Greenscreen23@users.noreply.github.com>
2023-02-07 22:56:35 +0100

    Add tests for the hospital editor

--- frontend/cypress/e2e/alarm-group.exercise.cy.ts
+++ frontend/cypress/e2e/alarm-group.exercise.cy.ts
@@ -5,7 +5,7 @@ describe('The alarm group overview on the exercise page', () => {
         cy.get('[data-cy=trainerToolbarAlarmGroupsButton]').click();
     });
 
-    afterEach(() => cy.deleteExercise())
+    afterEach(() => cy.deleteExercise());
 
     it('can create alarm groups', () => {
         cy.get('[data-cy="alarmGroupAddButton"]').click();
@@ -24,9 +24,12 @@ describe('The alarm group overview on the exercise page', () => {
 
     it('can rename alarm groups', () => {
         cy.get('[data-cy="alarmGroupAddButton"]').click();
-        cy.get('[data-cy="alarmGroupRenameInput"]').clear().type('ABC123')
+        cy.get('[data-cy="alarmGroupRenameInput"]')
+            .first()
+            .clear()
+            .type('ABC123');
 
-        cy.wait(1000)
+        cy.wait(1000);
         cy.performedActions()
             .then((a) => a.at(-1))
             .its('type')
@@ -37,12 +40,12 @@ describe('The alarm group overview on the exercise page', () => {
             .its('alarmGroups')
             .then((alarmGroups) => alarmGroups[Object.keys(alarmGroups)[0]!])
             .its('name')
-            .should('eq', 'ABC123')
-    })
+            .should('eq', 'ABC123');
+    });
 
     it('can remove alarm groups', () => {
         cy.get('[data-cy="alarmGroupAddButton"]').click();
-        cy.get('[data-cy="alarmGroupsRemoveButton"]').click();
+        cy.get('[data-cy="alarmGroupsRemoveButton"]').first().click();
 
         cy.performedActions()
             .then((a) => a.at(-1))
@@ -54,15 +57,15 @@ describe('The alarm group overview on the exercise page', () => {
             .its('alarmGroups')
             .then((alarmGroups) => Object.keys(alarmGroups))
             .its('length')
-            .should('eq', 0)
-    })
+            .should('eq', 0);
+    });
 
     it('can add vehicles to alarm groups', () => {
         cy.get('[data-cy="alarmGroupAddButton"]').click();
-        cy.get('[data-cy="alarmGroupAddVehicleButton"]').click();
-        cy.get(
-            '[data-cy="alarmGroupAddVehicleSelect"] > :nth-child(1)'
-        ).click();
+        cy.get('[data-cy="alarmGroupAddVehicleButton"]').first().click();
+        cy.get('[data-cy="alarmGroupAddVehicleSelect"] > :nth-child(1)')
+            .first()
+            .click();
 
         cy.performedActions()
             .then((a) => a.at(-1))
@@ -74,20 +77,23 @@ describe('The alarm group overview on the exercise page', () => {
             .its('alarmGroups')
             .then((alarmGroups) => alarmGroups[Object.keys(alarmGroups)[0]!])
             .its('alarmGroupVehicles')
-            .then(alarmGroupVehicles => Object.keys(alarmGroupVehicles))
+            .then((alarmGroupVehicles) => Object.keys(alarmGroupVehicles))
             .its('length')
             .should('eq', 1);
-    })
+    });
 
     it('can edit alarm group vehicles', () => {
         cy.get('[data-cy="alarmGroupAddButton"]').click();
-        cy.get('[data-cy="alarmGroupAddVehicleButton"]').click();
-        cy.get(
-            '[data-cy="alarmGroupAddVehicleSelect"] > :nth-child(1)'
-        ).click();
-        cy.get('[data-cy="alarmGroupVehicleDelayInput"]').clear().type('10');
+        cy.get('[data-cy="alarmGroupAddVehicleButton"]').first().click();
+        cy.get('[data-cy="alarmGroupAddVehicleSelect"] > :nth-child(1)')
+            .first()
+            .click();
+        cy.get('[data-cy="alarmGroupVehicleDelayInput"]')
+            .first()
+            .clear()
+            .type('10');
 
-        cy.wait(1000)
+        cy.wait(1000);
         cy.performedActions()
             .then((a) => a.at(-1))
             .its('type')
@@ -98,18 +104,21 @@ describe('The alarm group overview on the exercise page', () => {
             .its('alarmGroups')
             .then((alarmGroups) => alarmGroups[Object.keys(alarmGroups)[0]!])
             .its('alarmGroupVehicles')
-            .then(alarmGroupVehicles => alarmGroupVehicles[Object.keys(alarmGroupVehicles)[0]!])
+            .then(
+                (alarmGroupVehicles) =>
+                    alarmGroupVehicles[Object.keys(alarmGroupVehicles)[0]!]
+            )
             .its('time')
             .should('eq', 600000);
-    })
+    });
 
     it('can remove alarm group vehicles', () => {
         cy.get('[data-cy="alarmGroupAddButton"]').click();
-        cy.get('[data-cy="alarmGroupAddVehicleButton"]').click();
-        cy.get(
-            '[data-cy="alarmGroupAddVehicleSelect"] > :nth-child(1)'
-        ).click();
-        cy.get('[data-cy="alarmGroupRemoveVehicleButton"]').click();
+        cy.get('[data-cy="alarmGroupAddVehicleButton"]').first().click();
+        cy.get('[data-cy="alarmGroupAddVehicleSelect"] > :nth-child(1)')
+            .first()
+            .click();
+        cy.get('[data-cy="alarmGroupRemoveVehicleButton"]').first().click();
 
         cy.performedActions()
             .then((a) => a.at(-1))
@@ -121,8 +130,8 @@ describe('The alarm group overview on the exercise page', () => {
             .its('alarmGroups')
             .then((alarmGroups) => alarmGroups[Object.keys(alarmGroups)[0]!])
             .its('alarmGroupVehicles')
-            .then(alarmGroupVehicles => Object.keys(alarmGroupVehicles))
+            .then((alarmGroupVehicles) => Object.keys(alarmGroupVehicles))
             .its('length')
             .should('eq', 0);
-    })
+    });
 });
---
+++ frontend/cypress/e2e/hospital.exercise.cy.ts
@@ -0,0 +1,83 @@
+describe('The hospital overview on the exercise page', () => {
+    beforeEach(() => {
+        cy.createExercise().joinExerciseAsTrainer();
+        cy.get('[data-cy=trainerToolbarCreationButton]').click();
+        cy.get('[data-cy=trainerToolbarHospitalsButton]').click();
+    });
+
+    afterEach(() => cy.deleteExercise());
+
+    it('can create hospitals', () => {
+        cy.get('[data-cy="hospitalAddButton"]').click();
+        cy.performedActions()
+            .then((a) => a.at(-1))
+            .its('type')
+            .should('eq', '[Hospital] Add hospital');
+
+        cy.storeState()
+            .its('exerciseState')
+            .its('hospitals')
+            .then((hospitals) => Object.keys(hospitals))
+            .its('length')
+            .should('eq', 1);
+    });
+
+    it('can update a hospitals name', () => {
+        cy.get('[data-cy="hospitalAddButton"]').click();
+        cy.get('[data-cy="hospitalRenameInput"]')
+            .first()
+            .clear()
+            .type('ABC123');
+
+        cy.wait(1000);
+        cy.performedActions()
+            .then((a) => a.at(-1))
+            .its('type')
+            .should('eq', '[Hospital] Rename hospital');
+
+        cy.storeState()
+            .its('exerciseState')
+            .its('hospitals')
+            .then((hospitals) => hospitals[Object.keys(hospitals)[0]!])
+            .its('name')
+            .should('eq', 'ABC123');
+    });
+
+    it('can update a hospitals transport time', () => {
+        cy.get('[data-cy="hospitalAddButton"]').click();
+        cy.get('[data-cy="hospitalUpdateTransportTimeInput"]')
+            .first()
+            .clear()
+            .type('30');
+
+        cy.wait(1000);
+        cy.performedActions()
+            .then((a) => a.at(-1))
+            .its('type')
+            .should('eq', '[Hospital] Edit transportDuration to hospital');
+
+        cy.storeState()
+            .its('exerciseState')
+            .its('hospitals')
+            .then((hospitals) => hospitals[Object.keys(hospitals)[0]!])
+            .its('transportDuration')
+            .should('eq', 1800000);
+    });
+
+    it('can delete hospitals', () => {
+        cy.get('[data-cy="hospitalAddButton"]').click();
+        cy.get('[data-cy="hospitalDeleteButton"]').click();
+
+        cy.performedActions()
+            .then((a) => a.at(-1))
+            .its('type')
+            .should('eq', '[Hospital] Remove hospital');
+
+        cy.storeState()
+            .its('exerciseState')
+            .its('hospitals')
+            .then((hospitals) => Object.keys(hospitals))
+            .its('length')
+            .should('eq', 0);
+    });
+});
