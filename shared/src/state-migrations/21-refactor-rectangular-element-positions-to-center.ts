2b373dc8cb8b10915cf2509b5abb81d8387885f4
Julian Schmidt <julian.r.schmidt.js@gmail.com>
2023-02-16 23:48:46 +0100

    Improve the migration

--- shared/src/state-migrations/21-refactor-rectangular-element-positions-to-center.ts
+++ shared/src/state-migrations/21-refactor-rectangular-element-positions-to-center.ts
@@ -1,19 +1,39 @@
-import { ExerciseAction } from '../store';
-import type { Mutable } from '../utils';
+import { cloneDeep } from 'lodash-es';
+import { UUID } from '../utils';
 import type { Migration } from './migration-functions';
 
 export const refactorRectangularElementPositionsToCenter21: Migration = {
     actions: (_initialState, actions) => {
-        for (const action of actions as Mutable<ExerciseAction>[]) {
+        const oldViewportSizes: {
+            [viewportId: UUID]: {
+                width: number;
+                height: number;
+            };
+        } = {};
+        for (const action of actions as any) {
             switch (action?.type) {
                 case '[Viewport] Add viewport':
+                    if (action.viewport.position.type === 'coordinates') {
+                        oldViewportSizes[action.viewport.id] = cloneDeep(
+                            action.viewport.size
+                        );
+                    }
                     migrateRectangularElement(action.viewport);
                     break;
-                case '[Viewport] Move viewport':
-                case '[Viewport] Rename viewport':
-                    // It would be too much work to migrate these actions, as we need the current viewport size to convert them
-                    // This results in most replays having different viewport positions.
+                case '[Viewport] Move viewport': {
+                    const oldViewportSize = oldViewportSizes[action.viewportId];
+                    if (oldViewportSize) {
+                        migratePosition(action.targetPosition, oldViewportSize);
+                    }
                     break;
+                }
+                case '[Viewport] Resize viewport': {
+                    const oldViewportSize = cloneDeep(action.newSize);
+                    oldViewportSizes[action.viewportId] = oldViewportSize;
+                    migratePosition(action.targetPosition, oldViewportSize);
+                    migrateSize(action.newSize);
+                    break;
+                }
             }
         }
     },
@@ -27,13 +47,21 @@ export const refactorRectangularElementPositionsToCenter21: Migration = {
     },
 };
 
-function migrateRectangularElement(viewport: any) {
-    const { position, size } = viewport;
+function migrateRectangularElement(element: any) {
+    const { position, size } = element;
     if (position.type === 'coordinates') {
-        // The position was previously the top-left corner of the rectangle if the width and height was positive.
-        position.x = position.x + size.width / 2;
-        position.y = position.y - size.height / 2;
+        migratePosition(position.coordinates, size);
     }
+    migrateSize(size);
+}
+
+function migratePosition(position: any, oldSize: any) {
+    // The position was previously the top-left corner of the rectangle if the width and height was positive.
+    position.x = position.x + oldSize.width / 2;
+    position.y = position.y - oldSize.height / 2;
+}
+
+function migrateSize(size: any) {
     size.width = Math.abs(size.width);
     size.height = Math.abs(size.height);
 }
