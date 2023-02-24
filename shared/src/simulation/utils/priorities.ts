6083f38484f4abb067a1b066c36c7bdee6d0ff56
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-02-24 10:40:38 +0100

    Remove unnecessary code

--- shared/src/simulation/utils/priorities.ts
+++ shared/src/simulation/utils/priorities.ts
@@ -1,4 +1,4 @@
-import type { PatientStatus, PersonnelType } from '../../models/utils';
+import type { PersonnelType } from '../../models/utils';
 
 export const personnelPriorities: { [Key in PersonnelType]: number } = {
     gf: 0,
@@ -6,13 +6,4 @@ export const personnelPriorities: { [Key in PersonnelType]: number } = {
     rettSan: 2,
     notSan: 3,
     notarzt: 4,
-};
-
-export const patientPriorities: { [Key in PatientStatus]: number } = {
-    black: 0,
-    blue: 1,
-    green: 2,
-    yellow: 3,
-    red: 4,
-    white: 5,
 };
