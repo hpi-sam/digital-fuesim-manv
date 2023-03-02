c29e0fb5ae4f3fcb0d15af5a71bb3e64d7e80af6
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2023-03-02 14:55:06 +0100

    Rename simulated-region-item to simulated-region-overview-general

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview-general/simulated-region-overview-general.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview-general/simulated-region-overview-general.component.ts
@@ -3,11 +3,11 @@ import { SimulatedRegion } from 'digital-fuesim-manv-shared';
 import { ExerciseService } from 'src/app/core/exercise.service';
 
 @Component({
-    selector: 'app-simulated-region-item',
-    templateUrl: './simulated-region-item.component.html',
-    styleUrls: ['./simulated-region-item.component.scss'],
+    selector: 'app-simulated-region-overview-general',
+    templateUrl: './simulated-region-overview-general.component.html',
+    styleUrls: ['./simulated-region-overview-general.component.scss'],
 })
-export class SimulatedRegionItemComponent {
+export class SimulatedRegionOverviewGeneralComponent {
     @Input() simulatedRegion!: SimulatedRegion;
 
     constructor(private readonly exerciseService: ExerciseService) {}
--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview.module.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
-import { SimulatedRegionItemComponent } from './simulated-region-item/simulated-region-item.component';
+import { SimulatedRegionOverviewGeneralComponent } from './simulated-region-overview-general/simulated-region-overview-general.component';
 
 @NgModule({
-    declarations: [SimulatedRegionItemComponent],
+    declarations: [SimulatedRegionOverviewGeneralComponent],
     imports: [CommonModule, FormsModule, SharedModule, NgbNavModule],
-    exports: [SimulatedRegionItemComponent],
+    exports: [SimulatedRegionOverviewGeneralComponent],
 })
 export class SimulatedRegionOverviewModule {}
