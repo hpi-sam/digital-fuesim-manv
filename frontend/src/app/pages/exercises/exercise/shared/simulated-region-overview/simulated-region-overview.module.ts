491f4cee9aa1cf5f8607d7a35c61601098d3b849
Benildur <82985280+benn02@users.noreply.github.com>
2023-03-03 13:35:57 +0100

    Populate simulated region popup

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview.module.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
-import { SimulatedRegionOverviewGeneralComponent } from './simulated-region-overview-general/simulated-region-overview-general.component';
+import { SimulatedRegionOverviewGeneralComponent } from './overview/simulated-region-overview.component';
+import { BehaviorTabComponent } from './tabs/behavior-tab/behavior-tab.component';
+import { GeneralTabComponent } from './tabs/general-tab/general-tab.component';
+import { TransferTabComponent } from './tabs/transfer-tab/transfer-tab.component';
 
 @NgModule({
-    declarations: [SimulatedRegionOverviewGeneralComponent],
+    declarations: [
+        SimulatedRegionOverviewGeneralComponent,
+        BehaviorTabComponent,
+        GeneralTabComponent,
+        TransferTabComponent,
+    ],
     imports: [CommonModule, FormsModule, SharedModule, NgbNavModule],
     exports: [SimulatedRegionOverviewGeneralComponent],
 })
