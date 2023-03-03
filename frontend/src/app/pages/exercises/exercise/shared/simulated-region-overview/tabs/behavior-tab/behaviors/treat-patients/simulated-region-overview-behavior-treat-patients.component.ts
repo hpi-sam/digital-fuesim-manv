7f5171d02bfe23dd39d465302853f055b63ceaf2
Benildur <82985280+benn02@users.noreply.github.com>
2023-03-03 18:49:18 +0100

    Fix Lint

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/tabs/behavior-tab/behaviors/treat-patients/simulated-region-overview-behavior-treat-patients.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/tabs/behavior-tab/behaviors/treat-patients/simulated-region-overview-behavior-treat-patients.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-simulated-region-overview-behavior-treat-patients',
-  templateUrl: './simulated-region-overview-behavior-treat-patients.component.html',
-  styleUrls: ['./simulated-region-overview-behavior-treat-patients.component.scss']
+    selector: 'app-simulated-region-overview-behavior-treat-patients',
+    templateUrl:
+        './simulated-region-overview-behavior-treat-patients.component.html',
+    styleUrls: [
+        './simulated-region-overview-behavior-treat-patients.component.scss',
+    ],
 })
-export class SimulatedRegionOverviewBehaviorTreatPatientsComponent {
-
-}
+export class SimulatedRegionOverviewBehaviorTreatPatientsComponent {}
