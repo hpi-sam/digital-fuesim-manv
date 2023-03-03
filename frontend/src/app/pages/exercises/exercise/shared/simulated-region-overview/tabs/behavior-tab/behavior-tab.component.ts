6cef8f80722933ff5a2d7041e3b03b364e220fa3
Benildur <82985280+benn02@users.noreply.github.com>
2023-03-03 13:40:56 +0100

    Fix Lint

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/tabs/behavior-tab/behavior-tab.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/tabs/behavior-tab/behavior-tab.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-behavior-tab',
-  templateUrl: './behavior-tab.component.html',
-  styleUrls: ['./behavior-tab.component.scss']
+    selector: 'app-behavior-tab',
+    templateUrl: './behavior-tab.component.html',
+    styleUrls: ['./behavior-tab.component.scss'],
 })
-export class BehaviorTabComponent {
-
-}
+export class BehaviorTabComponent {}
--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/tabs/transfer-tab/transfer-tab.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/tabs/transfer-tab/transfer-tab.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-transfer-tab',
-  templateUrl: './transfer-tab.component.html',
-  styleUrls: ['./transfer-tab.component.scss']
+    selector: 'app-transfer-tab',
+    templateUrl: './transfer-tab.component.html',
+    styleUrls: ['./transfer-tab.component.scss'],
 })
-export class TransferTabComponent {
-
-}
+export class TransferTabComponent {}
