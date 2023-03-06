61b7911e095598c4fe2d4140c19672b437a233ab
Benildur <82985280+benn02@users.noreply.github.com>
2023-03-06 16:23:55 +0100

    Add Controls

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/tabs/behavior-tab/behaviors/treat-patients/simulated-region-overview-behavior-treat-patients.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/tabs/behavior-tab/behaviors/treat-patients/simulated-region-overview-behavior-treat-patients.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {
+    TreatPatientsBehaviorState,
+    SimulatedRegion,
+} from 'digital-fuesim-manv-shared';
+import { ExerciseService } from 'src/app/core/exercise.service';
 
 @Component({
     selector: 'app-simulated-region-overview-behavior-treat-patients',
@@ -8,4 +13,28 @@ import { Component } from '@angular/core';
         './simulated-region-overview-behavior-treat-patients.component.scss',
     ],
 })
-export class SimulatedRegionOverviewBehaviorTreatPatientsComponent {}
+export class SimulatedRegionOverviewBehaviorTreatPatientsComponent {
+    @Input() simulatedRegion!: SimulatedRegion;
+    @Input() treatPatientsBehaviorState!: TreatPatientsBehaviorState;
+
+    constructor(private readonly exerciseService: ExerciseService) {}
+
+    public updateTreatPatientsBehaviorState(
+        unknown: number,
+        counted: number,
+        triaged: number,
+        secured: number,
+        countingTimePerPatient: number
+    ) {
+        this.exerciseService.proposeAction({
+            type: '[Simulation] Update TreatPatientsIntervals',
+            simulatedRegionId: this.simulatedRegion.id,
+            behaviorStateId: this.treatPatientsBehaviorState.id,
+            unknown,
+            counted,
+            triaged,
+            secured,
+            countingTimePerPatient,
+        });
+    }
+}
