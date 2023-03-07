f3a6a2aa26fe1a2f064ee2c5736ca4048abf8b09
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-03-07 12:51:05 +0100

    Display the current leader type if there is one

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/tabs/behavior-tab/behaviors/assign-leader/simulated-region-overview-behavior-assign-leader.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/tabs/behavior-tab/behaviors/assign-leader/simulated-region-overview-behavior-assign-leader.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import type { OnChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import type { Personnel } from 'digital-fuesim-manv-shared';
+import {
+    AssignLeaderBehaviorState,
+    SimulatedRegion,
+} from 'digital-fuesim-manv-shared';
+import type { Observable } from 'rxjs';
+import type { AppState } from 'src/app/state/app.state';
+import { createSelectPersonnel } from 'src/app/state/application/selectors/exercise.selectors';
 
 @Component({
     selector: 'app-simulated-region-overview-behavior-assign-leader',
@@ -8,4 +18,24 @@ import { Component } from '@angular/core';
         './simulated-region-overview-behavior-assign-leader.component.scss',
     ],
 })
-export class SimulatedRegionOverviewBehaviorAssignLeaderComponent {}
+export class SimulatedRegionOverviewBehaviorAssignLeaderComponent
+    implements OnChanges
+{
+    @Input()
+    simulatedRegion!: SimulatedRegion;
+
+    @Input()
+    assignLeaderBehaviorState!: AssignLeaderBehaviorState;
+
+    currentLeader?: Observable<Personnel>;
+
+    constructor(private readonly store: Store<AppState>) {}
+
+    ngOnChanges(): void {
+        if (this.assignLeaderBehaviorState.leaderId) {
+            this.currentLeader = this.store.select(
+                createSelectPersonnel(this.assignLeaderBehaviorState.leaderId)
+            );
+        }
+    }
+}
