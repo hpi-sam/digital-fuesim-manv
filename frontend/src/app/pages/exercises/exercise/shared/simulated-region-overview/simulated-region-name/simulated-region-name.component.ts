57a34f765336a31078abbac91a7348df3cf7087b
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-03-09 10:32:08 +0100

    Create a popup that shows all simulated regions (#729)
    * WIP
    
    * Fix layout
    
    * Use pipe instead of selector
    
    * Prevent unnecessary redrawing
    
    * Remove unnecessary `distinctUntilChanges`
    
    * Allow specifying an initially selected simreg
    
    * Update changelog

---
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-name/simulated-region-name.component.ts
@@ -0,0 +1,27 @@
+import type { OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { UUID } from 'digital-fuesim-manv-shared';
+import type { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import type { AppState } from 'src/app/state/app.state';
+import { createSelectSimulatedRegion } from 'src/app/state/application/selectors/exercise.selectors';
+
+@Component({
+    selector: 'app-simulated-region-name',
+    templateUrl: './simulated-region-name.component.html',
+    styleUrls: ['./simulated-region-name.component.scss'],
+})
+export class SimulatedRegionNameComponent implements OnInit {
+    @Input() simulatedRegionId!: UUID;
+
+    name$!: Observable<string>;
+
+    constructor(private readonly store: Store<AppState>) {}
+
+    ngOnInit(): void {
+        this.name$ = this.store
+            .select(createSelectSimulatedRegion(this.simulatedRegionId))
+            .pipe(map((simulatedRegion) => simulatedRegion.name));
+    }
+}
