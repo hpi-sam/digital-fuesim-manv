4fa2b76ca7234868096b40f0a04dad3e71b5a85f
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-03-10 13:25:41 +0100

    Show details of patients in simulated region (#739)
    * Add patients tab
    
    * Extract health badge
    
    * Extract patient name and patient details
    
    * Embed patient details in simulated region popup
    
    * Sort patients based on their visible status
    
    * Make patient details reachable from treatment behavior
    
    * Remove old code
    
    * Order `patientCategoryOrderDictionary`
    
    * Remove old property from patients tab component
    
    * Update changelog
    
    * Fix number of tabs in changelog

---
+++ frontend/src/app/pages/exercises/exercise/shared/exercise-map/shared/patient-name/patient-name.component.ts
@@ -0,0 +1,25 @@
+import type { OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import type { Patient } from 'digital-fuesim-manv-shared';
+import { UUID } from 'digital-fuesim-manv-shared';
+import type { Observable } from 'rxjs';
+import type { AppState } from 'src/app/state/app.state';
+import { createSelectPatient } from 'src/app/state/application/selectors/exercise.selectors';
+
+@Component({
+    selector: 'app-patient-name',
+    templateUrl: './patient-name.component.html',
+    styleUrls: ['./patient-name.component.scss'],
+})
+export class PatientNameComponent implements OnInit {
+    @Input() patientId!: UUID;
+
+    patient$!: Observable<Patient>;
+
+    constructor(private readonly store: Store<AppState>) {}
+
+    ngOnInit(): void {
+        this.patient$ = this.store.select(createSelectPatient(this.patientId));
+    }
+}
