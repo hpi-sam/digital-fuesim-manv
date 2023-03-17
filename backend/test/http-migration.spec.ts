95e3ea49902c2bfa8ee9f9ae65d1cb08756cc9bc
Benildur <82985280+benn02@users.noreply.github.com>
2023-03-17 13:46:31 +0100

    WIP

---
+++ backend/test/http-migration.spec.ts
@@ -0,0 +1,36 @@
+import fs from 'node:fs';
+import { UserReadableIdGenerator } from 'utils/user-readable-id-generator';
+import { exerciseMap } from 'exercise/exercise-map';
+import { createTestEnvironment } from './utils';
+
+const basePath =
+    'C:/Users/benil/Documents/Uni/BP/Test Scenarios/digital-fuesim-manv-public-test-scenarios/migration-test-scenarios';
+
+describe('migration', () => {
+    const environment = createTestEnvironment();
+
+    beforeEach(async () => {
+        UserReadableIdGenerator.freeAll();
+        exerciseMap.clear();
+    });
+
+    fs.readdirSync(basePath).forEach((dir) => {
+        describe(dir, () => {
+            test.each(fs.readdirSync(`${basePath }/${ dir}`))(
+                'It imports %s correctly',
+                async (exercisePath) => {
+                    const exercise = JSON.parse(
+                        fs.readFileSync(
+                            `${basePath }/${ dir}/${exercisePath}`,
+                            'utf8'
+                        )
+                    );
+                    await environment
+                        .httpRequest('post', '/api/exercise')
+                        .send(exercise)
+                        .expect(201);
+                }
+            );
+        });
+    });
+});
