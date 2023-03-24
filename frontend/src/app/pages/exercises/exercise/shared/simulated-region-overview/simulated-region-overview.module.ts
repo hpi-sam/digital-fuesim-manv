c6a10e726413a47946cad76f84ce8b29c3949ff2
Nils1729 <45318774+Nils1729@users.noreply.github.com>
2023-03-24 16:43:40 +0100

    Add radiogram component for treatment status radiograms (#784)
    * Add radiogram component for treatment status radiograms
    
    * Update frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/radiogram-list/radiogram-card/radiogram-card-content-treatment-status/radiogram-card-content-treatment-status.component.html
    
    Co-authored-by: Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
    
    * Add display for noTreatment
    
    * lint
    
    ---------
    
    Co-authored-by: Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview.module.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview.module.ts
@@ -29,6 +29,7 @@ import { RadiogramListComponent } from './radiogram-list/radiogram-list.componen
 import { RadiogramCardComponent } from './radiogram-list/radiogram-card/radiogram-card.component';
 import { RadiogramCardContentComponent } from './radiogram-list/radiogram-card/radiogram-card-content/radiogram-card-content.component';
 import { RadiogramCardContentFallbackComponent } from './radiogram-list/radiogram-card/radiogram-card-content-fallback/radiogram-card-content-fallback.component';
+import { RadiogramCardContentTreatmentStatusComponent } from './radiogram-list/radiogram-card/radiogram-card-content-treatment-status/radiogram-card-content-treatment-status.component';
 import { RadiogramCardContentPersonnelCountComponent } from './radiogram-list/radiogram-card/radiogram-card-content-personnel-count/radiogram-card-content-personnel-count.component';
 import { SimulatedRegionOverviewBehaviorReportComponent } from './tabs/behavior-tab/behaviors/report/simulated-region-overview-behavior-report.component';
 
@@ -52,6 +53,7 @@ import { SimulatedRegionOverviewBehaviorReportComponent } from './tabs/behavior-
         RadiogramCardComponent,
         RadiogramCardContentComponent,
         RadiogramCardContentFallbackComponent,
+        RadiogramCardContentTreatmentStatusComponent,
         RadiogramCardContentPersonnelCountComponent,
         SimulatedRegionOverviewBehaviorReportComponent,
     ],
