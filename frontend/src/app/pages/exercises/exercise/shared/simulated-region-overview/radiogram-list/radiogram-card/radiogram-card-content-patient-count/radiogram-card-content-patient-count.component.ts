d8df0eb265b3e4117d7f47a773642cf66e99b270
Benildur <82985280+benn02@users.noreply.github.com>
2023-03-24 17:09:51 +0100

    Respond to Feedback

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/radiogram-list/radiogram-card/radiogram-card-content-patient-count/radiogram-card-content-patient-count.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/radiogram-list/radiogram-card/radiogram-card-content-patient-count/radiogram-card-content-patient-count.component.ts
@@ -1,10 +1,18 @@
 import type { OnInit } from '@angular/core';
 import { Component, Input } from '@angular/core';
+import type { MemoizedSelector } from '@ngrx/store';
 import { createSelector, Store } from '@ngrx/store';
-import { PatientCountRadiogram } from 'digital-fuesim-manv-shared';
+import type {
+    ExerciseRadiogram,
+    PatientCountRadiogram,
+} from 'digital-fuesim-manv-shared';
+import { UUID } from 'digital-fuesim-manv-shared';
 import type { Observable } from 'rxjs';
 import type { AppState } from 'src/app/state/app.state';
-import { selectConfiguration } from 'src/app/state/application/selectors/exercise.selectors';
+import {
+    createSelectRadiogram,
+    selectConfiguration,
+} from 'src/app/state/application/selectors/exercise.selectors';
 
 @Component({
     selector: 'app-radiogram-card-content-patient-count',
@@ -12,25 +20,42 @@ import { selectConfiguration } from 'src/app/state/application/selectors/exercis
     styleUrls: ['./radiogram-card-content-patient-count.component.scss'],
 })
 export class RadiogramCardContentPatientCountComponent implements OnInit {
-    @Input() radiogram!: PatientCountRadiogram;
+    @Input() radiogramId!: UUID;
+    radiogram$!: Observable<PatientCountRadiogram>;
     bluePatientsEnabled$!: Observable<boolean>;
-    totalPatientCount!: number;
+    totalPatientCount$!: Observable<number>;
 
     constructor(private readonly store: Store<AppState>) {}
 
     ngOnInit(): void {
-        this.totalPatientCount =
-            this.radiogram.patientCount.black +
-            this.radiogram.patientCount.white +
-            this.radiogram.patientCount.red +
-            this.radiogram.patientCount.yellow +
-            this.radiogram.patientCount.green +
-            this.radiogram.patientCount.blue;
+        const radiogramSelector = createSelectRadiogram(
+            this.radiogramId
+        ) as MemoizedSelector<
+            AppState,
+            NonNullable<PatientCountRadiogram>,
+            (s1: {
+                [key: string]: ExerciseRadiogram;
+            }) => NonNullable<PatientCountRadiogram>
+        >;
+
+        const totalPatientCountSelector = createSelector(
+            radiogramSelector,
+            (radiogram) =>
+                radiogram.patientCount.black +
+                radiogram.patientCount.white +
+                radiogram.patientCount.red +
+                radiogram.patientCount.yellow +
+                radiogram.patientCount.green +
+                radiogram.patientCount.blue
+        );
 
         const bluePatientsEnabledSelector = createSelector(
             selectConfiguration,
             (configuration) => configuration.bluePatientsEnabled
         );
+
+        this.radiogram$ = this.store.select(radiogramSelector);
+        this.totalPatientCount$ = this.store.select(totalPatientCountSelector);
         this.bluePatientsEnabled$ = this.store.select(
             bluePatientsEnabledSelector
         );
