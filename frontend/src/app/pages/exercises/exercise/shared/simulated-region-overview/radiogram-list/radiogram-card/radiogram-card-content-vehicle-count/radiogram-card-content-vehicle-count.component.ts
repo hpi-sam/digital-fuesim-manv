4cedd3bea9da0e9e1e2bef548aa3c814952e4b69
Benildur <82985280+benn02@users.noreply.github.com>
2023-03-24 15:37:10 +0100

    Respond to feedback

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/radiogram-list/radiogram-card/radiogram-card-content-vehicle-count/radiogram-card-content-vehicle-count.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/radiogram-list/radiogram-card/radiogram-card-content-vehicle-count/radiogram-card-content-vehicle-count.component.ts
@@ -1,10 +1,7 @@
 import type { OnInit } from '@angular/core';
 import { Component, Input } from '@angular/core';
-import { createSelector, Store } from '@ngrx/store';
 import { VehicleCountRadiogram } from 'digital-fuesim-manv-shared';
 import type { Observable } from 'rxjs';
-import type { AppState } from 'src/app/state/app.state';
-import { createSelectSimulatedRegion } from 'src/app/state/application/selectors/exercise.selectors';
 
 @Component({
     selector: 'app-radiogram-card-content-vehicle-count',
@@ -15,28 +12,15 @@ export class RadiogramCardContentVehicleCountComponent implements OnInit {
     @Input() radiogram!: VehicleCountRadiogram;
     totalVehicleCount!: number;
     simulatedRegionName$!: Observable<string>;
-    vehicleCounts!: { vehicleName: string; vehicleCount: number }[];
-
-    constructor(private readonly store: Store<AppState>) {}
+    vehicleCounts!: { vehicleType: string; vehicleCount: number }[];
 
     ngOnInit(): void {
         this.vehicleCounts = Object.entries(this.radiogram.vehicleCount).map(
-            ([vehicleName, vehicleCount]) => ({ vehicleName, vehicleCount })
+            ([vehicleType, vehicleCount]) => ({ vehicleType, vehicleCount })
         );
 
         this.totalVehicleCount = Object.values(
             this.radiogram.vehicleCount
         ).reduce((a, b) => a + b, 0);
-
-        const simulatedRegionSelector = createSelectSimulatedRegion(
-            this.radiogram.simulatedRegionId
-        );
-        const simulatedRegionNameSelector = createSelector(
-            simulatedRegionSelector,
-            (simulatedRegion) => simulatedRegion.name
-        );
-        this.simulatedRegionName$ = this.store.select(
-            simulatedRegionNameSelector
-        );
     }
 }
