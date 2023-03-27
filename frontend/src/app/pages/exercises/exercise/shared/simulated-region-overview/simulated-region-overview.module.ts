738d307000b755b9152496824eb8bb32876876e2
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-03-27 13:05:58 +0200

    Mark empty radiograms (#811)

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview.module.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview.module.ts
@@ -35,6 +35,8 @@ import { RadiogramCardContentPatientCountComponent } from './radiogram-list/radi
 import { RadiogramCardContentPersonnelCountComponent } from './radiogram-list/radiogram-card/radiogram-card-content-personnel-count/radiogram-card-content-personnel-count.component';
 import { RadiogramCardContentVehicleCountComponent } from './radiogram-list/radiogram-card/radiogram-card-content-vehicle-count/radiogram-card-content-vehicle-count.component';
 import { SimulatedRegionOverviewBehaviorReportComponent } from './tabs/behavior-tab/behaviors/report/simulated-region-overview-behavior-report.component';
+import { RadiogramCardContentInformationUnavailableComponent } from './radiogram-list/radiogram-card/radiogram-card-content-information-unavailable/radiogram-card-content-information-unavailable.component';
+import { HumanReadableRadiogramTypePipe } from './radiogram-list/human-readable-radiogram-type.pipe';
 import { TreatmentStatusBadgeComponent } from './treatment-status-badge/treatment-status-badge.component';
 
 @NgModule({
@@ -63,6 +65,8 @@ import { TreatmentStatusBadgeComponent } from './treatment-status-badge/treatmen
         RadiogramCardContentPersonnelCountComponent,
         RadiogramCardContentVehicleCountComponent,
         SimulatedRegionOverviewBehaviorReportComponent,
+        RadiogramCardContentInformationUnavailableComponent,
+        HumanReadableRadiogramTypePipe,
         TreatmentStatusBadgeComponent,
     ],
     imports: [
