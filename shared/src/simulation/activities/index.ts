ca18cea48a3a1fbcc25146480db2021f85520ce5
Benildur <82985280+benn02@users.noreply.github.com>
2023-03-29 16:16:34 +0200

    Add Transfer Vehicles Activity

--- shared/src/simulation/activities/index.ts
+++ shared/src/simulation/activities/index.ts
@@ -3,3 +3,4 @@ export * from './exercise-simulation-activity';
 export * from './reassign-treatments';
 export * from './unload-vehicle';
 export * from './recurring-event';
+export * from './transfer-vehicles';
---
+++ shared/src/simulation/activities/transfer-vehicles.ts
@@ -0,0 +1,190 @@
+import { Type } from 'class-transformer';
+import { IsString, IsUUID, ValidateNested } from 'class-validator';
+import { groupBy } from 'lodash-es';
+import {
+    MissingTransferConnectionRadiogram,
+    RadiogramUnpublishedStatus,
+} from '../../models/radiogram';
+import { publishRadiogram } from '../../models/radiogram/radiogram-helpers-mutable';
+import {
+    currentSimulatedRegionIdOf,
+    getCreate,
+    isInSimulatedRegion,
+    isInSpecificSimulatedRegion,
+    TransferStartPoint,
+} from '../../models/utils';
+import { VehicleResource } from '../../models/utils/vehicle-resource';
+import { TransferActionReducers } from '../../store/action-reducers/transfer';
+import { getElement } from '../../store/action-reducers/utils';
+import { cloneDeepMutable, UUID, uuidValidationOptions } from '../../utils';
+import { IsValue } from '../../utils/validators';
+import {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    ExerciseSimulationEvent,
+    ResourceRequiredEvent,
+    VehiclesSentEvent,
+} from '../events';
+import { sendSimulationEvent } from '../events/utils';
+import { nextUUID } from '../utils/randomness';
+import type {
+    SimulationActivity,
+    SimulationActivityState,
+} from './simulation-activity';
+
+export class TransferVehiclesActivityState implements SimulationActivityState {
+    @IsValue('transferVehiclesActivity' as const)
+    public readonly type = 'transferVehiclesActivity';
+
+    @IsUUID(4, uuidValidationOptions)
+    public readonly id: UUID;
+
+    @IsUUID(4, uuidValidationOptions)
+    public readonly targetTransferPointId: UUID;
+
+    @IsString()
+    public readonly key: string;
+
+    @ValidateNested()
+    @Type(() => VehicleResource)
+    public readonly vehiclesToBeTransferred: VehicleResource;
+
+    /**
+     * @deprecated Use {@link create} instead
+     */
+    constructor(
+        id: UUID,
+        targetTransferPointId: UUID,
+        key: string,
+        vehiclesToBeTransferred: VehicleResource
+    ) {
+        this.id = id;
+        this.targetTransferPointId = targetTransferPointId;
+        this.key = key;
+        this.vehiclesToBeTransferred = vehiclesToBeTransferred;
+    }
+
+    static readonly create = getCreate(this);
+}
+
+export const transferVehiclesActivity: SimulationActivity<TransferVehiclesActivityState> =
+    {
+        activityState: TransferVehiclesActivityState,
+        tick(
+            draftState,
+            simulatedRegion,
+            activityState,
+            _tickInterval,
+            terminate
+        ) {
+            const ownTransferPoint = Object.values(
+                draftState.transferPoints
+            ).find((transferPoint) =>
+                isInSpecificSimulatedRegion(transferPoint, simulatedRegion.id)
+            )!;
+
+            if (
+                !Object.keys(ownTransferPoint.reachableTransferPoints).includes(
+                    activityState.targetTransferPointId
+                )
+            ) {
+                publishRadiogram(
+                    draftState,
+                    cloneDeepMutable(
+                        MissingTransferConnectionRadiogram.create(
+                            nextUUID(draftState),
+                            simulatedRegion.id,
+                            RadiogramUnpublishedStatus.create(),
+                            activityState.targetTransferPointId
+                        )
+                    )
+                );
+                terminate();
+                return;
+            }
+
+            const vehicles = Object.values(draftState.vehicles).filter(
+                (vehicle) =>
+                    isInSpecificSimulatedRegion(vehicle, simulatedRegion.id)
+            );
+            const groupedVehicles = groupBy(
+                vehicles,
+                (vehicle) => vehicle.vehicleType
+            );
+
+            if (
+                Object.entries(
+                    activityState.vehiclesToBeTransferred.vehicleCounts
+                ).some(
+                    ([vehicleType, vehicleCount]) =>
+                        (groupedVehicles[vehicleType]?.length ?? 0) <
+                        vehicleCount
+                )
+            ) {
+                const missingVehicles: { [key: string]: number } = {};
+                Object.entries(
+                    activityState.vehiclesToBeTransferred.vehicleCounts
+                ).forEach(([vehicleType, vehicleCount]) => {
+                    if (
+                        (groupedVehicles[vehicleType]?.length ?? 0) <
+                        vehicleCount
+                    ) {
+                        missingVehicles[vehicleType] =
+                            vehicleCount -
+                            (groupedVehicles[vehicleType]?.length ?? 0);
+                    }
+                });
+                sendSimulationEvent(
+                    simulatedRegion,
+                    ResourceRequiredEvent.create(
+                        nextUUID(draftState),
+                        simulatedRegion.id,
+                        VehicleResource.create(missingVehicles),
+                        `transfer-vehicles-requirement-${nextUUID(draftState)}`
+                    )
+                );
+            }
+
+            const sentVehicles: { [key: string]: number } = {};
+            Object.entries(
+                activityState.vehiclesToBeTransferred.vehicleCounts
+            ).forEach(([vehicleType, vehicleCount]) => {
+                sentVehicles[vehicleType] = Math.min(
+                    groupedVehicles[vehicleType]?.length ?? 0,
+                    vehicleCount
+                );
+                for (let i = 0; i < sentVehicles[vehicleType]!; i++) {
+                    TransferActionReducers.addToTransfer.reducer(draftState, {
+                        type: '[Transfer] Add to transfer',
+                        elementType: 'vehicle',
+                        elementId: groupedVehicles[vehicleType]![i]!.id,
+                        startPoint: TransferStartPoint.create(
+                            ownTransferPoint.id
+                        ),
+                        targetTransferPointId:
+                            activityState.targetTransferPointId,
+                    });
+                }
+            });
+
+            const targetTransferPoint = getElement(
+                draftState,
+                'transferPoint',
+                activityState.targetTransferPointId
+            );
+            if (isInSimulatedRegion(targetTransferPoint)) {
+                sendSimulationEvent(
+                    getElement(
+                        draftState,
+                        'simulatedRegion',
+                        currentSimulatedRegionIdOf(targetTransferPoint)
+                    ),
+                    VehiclesSentEvent.create(
+                        nextUUID(draftState),
+                        VehicleResource.create(sentVehicles)
+                    )
+                );
+            }
+
+            terminate();
+        },
+    };
