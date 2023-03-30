e7d430bba33d3e864d01750a791d93dee7c94848
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2023-03-30 12:23:36 +0200

    Add vehicle request radiogram component

--- frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview.module.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulated-region-overview/simulated-region-overview.module.ts
@@ -39,6 +39,7 @@ import { RadiogramCardContentInformationUnavailableComponent } from './radiogram
 import { HumanReadableRadiogramTypePipe } from './radiogram-list/human-readable-radiogram-type.pipe';
 import { TreatmentStatusBadgeComponent } from './treatment-status-badge/treatment-status-badge.component';
 import { RadigoramCardContentMissingTransferConnectionComponent } from './radiogram-list/radiogram-card/radigoram-card-content-missing-transfer-connection/radigoram-card-content-missing-transfer-connection.component';
+import { RadigoramCardContentResourceRequestComponent } from './radiogram-list/radiogram-card/radigoram-card-content-resource-request/radigoram-card-content-resource-request.component';
 
 @NgModule({
     declarations: [
@@ -70,6 +71,7 @@ import { RadigoramCardContentMissingTransferConnectionComponent } from './radiog
         HumanReadableRadiogramTypePipe,
         TreatmentStatusBadgeComponent,
         RadigoramCardContentMissingTransferConnectionComponent,
+        RadigoramCardContentResourceRequestComponent,
     ],
     imports: [
         CommonModule,
