eb027a3529b294379cc2643363ad7e736991255c
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2023-04-11 11:37:08 +0200

    Introduce circular dependency

--- shared/src/models/radiogram/status/radiogram-accepted-status.ts
+++ shared/src/models/radiogram/status/radiogram-accepted-status.ts
@@ -1,7 +1,7 @@
 import { IsInt, IsUUID, Min } from 'class-validator';
 import { UUID, uuidValidationOptions } from '../../../utils';
 import { IsValue } from '../../../utils/validators';
-import { getCreate } from '../../utils/get-create';
+import { getCreate } from '../..';
 import type { RadiogramStatus } from './radiogram-status';
 
 export class RadiogramAcceptedStatus implements RadiogramStatus {
