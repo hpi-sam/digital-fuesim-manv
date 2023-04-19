520f3b9fbc9c0d843c0e1412d133d9c2892d449b
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2023-04-19 10:16:09 +0200

    Invalidate requestedResources after receiving vehicles

--- shared/src/simulation/behaviors/request.ts
+++ shared/src/simulation/behaviors/request.ts
@@ -8,12 +8,12 @@ import {
 } from 'class-validator';
 import { Type } from 'class-transformer';
 import { IsStringMap } from '../../utils/validators/is-string-map';
-import { cloneDeepMutable, StrictObject, uuid, UUID } from '../../utils';
+import { cloneDeepMutable, StrictObject, uuid, UUID, uuidValidationOptions } from '../../utils';
 import type { Mutable } from '../../utils';
 import { IsValue } from '../../utils/validators';
 import { getActivityById, getElement } from '../../store/action-reducers/utils';
 import type { ExerciseState } from '../../state';
-import { addActivity } from '../activities/utils';
+import { addActivity, terminateActivity } from '../activities/utils';
 import { nextUUID } from '../utils/randomness';
 import { RecurringEventActivityState } from '../activities';
 import { SendRequestEvent } from '../events/send-request';
@@ -146,6 +146,8 @@ export const requestBehavior: SimulationBehavior<RequestBehaviorState> = {
                             Object.keys(promise.resource.vehicleCounts).length >
                             0
                     );
+
+                behaviorState.requestedResources = {}
                 break;
             }
             case 'sendRequestEvent': {
