289bb24af7d1f1a1b4c76f270737a8afcfedcda9
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-04-25 15:27:22 +0200

    Add vehicles tab (#903)
    * Add empty vehicles tab
    
    * Show list of vehicles grouped by vehicleType
    
    * Extract components from vehicle popup
    
    * Remove unnecessary async modifier
    
    * Enable unloading in simulated regions
    
    * Send removed events when loading vehicle
    
    * Place label in upper component
    
    * Use OnChanges instead of OnInit
    
    * Extract patient slots display
    
    * Extract patients table
    
    * Show vehicle details (name, personnel, patients)
    
    * Add buttons for move and remove
    
    * Enable deletion of vehicles
    
    * Enable moving vehicles to the map
    
    * Add changes to changelog
    
    * Add test scenarios for vehicles tab
    
    * Restrict tab header text length

---
+++ frontend/src/app/shared/components/vehicle-name-editor/vehicle-name-editor.component.ts
@@ -0,0 +1,38 @@
+import type { OnChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import type { Vehicle } from 'digital-fuesim-manv-shared';
+import { UUID } from 'digital-fuesim-manv-shared';
+import type { Observable } from 'rxjs';
+import { ExerciseService } from 'src/app/core/exercise.service';
+import type { AppState } from 'src/app/state/app.state';
+import { createSelectVehicle } from 'src/app/state/application/selectors/exercise.selectors';
+
+@Component({
+    selector: 'app-vehicle-name-editor',
+    templateUrl: './vehicle-name-editor.component.html',
+    styleUrls: ['./vehicle-name-editor.component.scss'],
+})
+export class VehicleNameEditorComponent implements OnChanges {
+    @Input()
+    vehicleId!: UUID;
+
+    vehicle$!: Observable<Vehicle>;
+
+    constructor(
+        private readonly store: Store<AppState>,
+        private readonly exerciseService: ExerciseService
+    ) {}
+
+    ngOnChanges() {
+        this.vehicle$ = this.store.select(createSelectVehicle(this.vehicleId));
+    }
+
+    public renameVehicle(name: string) {
+        this.exerciseService.proposeAction({
+            type: '[Vehicle] Rename vehicle',
+            vehicleId: this.vehicleId,
+            name,
+        });
+    }
+}
