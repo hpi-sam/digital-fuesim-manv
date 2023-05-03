b8768f161bf7a23212e37ab2c5ce3fe5b33262c0
Benildur <82985280+benn02@users.noreply.github.com>
2023-05-03 18:09:42 +0200

    Improve migration

--- shared/src/state-migrations/29-remove-transfer-vehicles-activity-and-change-answer-request-behavior.ts
+++ shared/src/state-migrations/29-remove-transfer-vehicles-activity-and-change-answer-request-behavior.ts
@@ -2,7 +2,74 @@ import type { Migration } from './migration-functions';
 
 export const removeTransferVehiclesActivityAndChangeAnswerRequestBehavior29: Migration =
     {
-        action: null,
+        action: (_intermediaryState, action) => {
+            if (
+                (action as { type: string }).type ===
+                '[SimulatedRegion] Add Behavior'
+            ) {
+                const typedAction = action as {
+                    behaviorState:
+                        | {
+                              type: 'answerRequestsBehavior';
+                              receivedEvents: unknown[] | undefined;
+                              requestsHandled: number | undefined;
+                          }
+                        | {
+                              type: Exclude<'answerRequestsBehavior', unknown>;
+                          };
+                };
+                if (
+                    typedAction.behaviorState.type === 'answerRequestsBehavior'
+                ) {
+                    typedAction.behaviorState.receivedEvents = [];
+                    typedAction.behaviorState.requestsHandled = 0;
+                }
+            } else if (
+                (action as { type: string }).type ===
+                '[SimulatedRegion] Add simulated region'
+            ) {
+                const typedAction = action as {
+                    simulatedRegion: {
+                        activities: {
+                            [stateId: string]: {
+                                type: string | 'transferVehiclesActivity';
+                            };
+                        };
+                        behaviors: (
+                            | {
+                                  type: 'answerRequestsBehavior';
+                                  receivedEvents: unknown[] | undefined;
+                                  requestsHandled: number | undefined;
+                              }
+                            | {
+                                  type: Exclude<
+                                      'answerRequestsBehavior',
+                                      unknown
+                                  >;
+                              }
+                        )[];
+                    };
+                };
+                Object.keys(typedAction.simulatedRegion.activities).forEach(
+                    (key) => {
+                        if (
+                            typedAction.simulatedRegion.activities[key]!
+                                .type === 'transferVehiclesActivity'
+                        ) {
+                            delete typedAction.simulatedRegion.activities[key];
+                        }
+                    }
+                );
+
+                typedAction.simulatedRegion.behaviors.forEach((behavior) => {
+                    if (behavior.type === 'answerRequestsBehavior') {
+                        behavior.receivedEvents = [];
+                        behavior.requestsHandled = 0;
+                    }
+                });
+            }
+            return true;
+        },
         state: (state) => {
             const typedState = state as {
                 simulatedRegions: {
