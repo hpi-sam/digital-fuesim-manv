254527b3facc2f5b55f297e73c5f5a7cce6cd30b
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-05-23 15:04:06 +0200

    Add keyboard navigation to searchable dropdown

--- frontend/src/app/shared/components/searchable-dropdown/searchable-dropdown.component.ts
+++ frontend/src/app/shared/components/searchable-dropdown/searchable-dropdown.component.ts
@@ -1,15 +1,24 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import type { AfterViewInit } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    EventEmitter,
+    Input,
+    Output,
+    ViewChild,
+} from '@angular/core';
 
 @Component({
     selector: 'app-searchable-dropdown',
     templateUrl: './searchable-dropdown.component.html',
     styleUrls: ['./searchable-dropdown.component.scss'],
 })
-export class SearchableDropdownComponent {
+export class SearchableDropdownComponent implements AfterViewInit {
     @Input()
     public options: string[] = [];
 
     public filter = '';
+    public selectedIndex = -1;
 
     public get filteredOptions() {
         return this.options.filter((option) =>
@@ -19,4 +28,37 @@ export class SearchableDropdownComponent {
 
     @Output()
     public readonly selected: EventEmitter<string> = new EventEmitter();
+
+    @ViewChild('searchInput')
+    private readonly searchInput!: ElementRef;
+
+    ngAfterViewInit() {
+        this.searchInput.nativeElement.focus();
+    }
+
+    increaseSelectedIndex() {
+        if (this.selectedIndex + 1 < this.filteredOptions.length)
+            this.selectedIndex++;
+    }
+
+    decreaseSelectedIndex() {
+        if (this.selectedIndex - 1 >= -1) this.selectedIndex--;
+    }
+
+    resetSelectedIndex() {
+        if (this.filteredOptions.length === 1) {
+            this.selectedIndex = 0;
+        } else {
+            this.selectedIndex = -1;
+        }
+    }
+
+    confirmSelection() {
+        if (
+            this.selectedIndex > -1 &&
+            this.selectedIndex < this.filteredOptions.length
+        ) {
+            this.selected.emit(this.filteredOptions[this.selectedIndex]);
+        }
+    }
 }
