c84495b00e7b565151f19ec472fbd224c1e89ff1
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-06-22 13:28:42 +0200

    Run prettier
    Signed-off-by: Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>

--- frontend/src/app/pages/exercises/exercise/shared/simulation/signaller-modal/signaller-modal-region-commands/signaller-modal-region-commands.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulation/signaller-modal/signaller-modal-region-commands/signaller-modal-region-commands.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-signaller-modal-region-commands',
-  templateUrl: './signaller-modal-region-commands.component.html',
-  styleUrls: ['./signaller-modal-region-commands.component.scss']
+    selector: 'app-signaller-modal-region-commands',
+    templateUrl: './signaller-modal-region-commands.component.html',
+    styleUrls: ['./signaller-modal-region-commands.component.scss'],
 })
-export class SignallerModalRegionCommandsComponent {
-
-}
+export class SignallerModalRegionCommandsComponent {}
