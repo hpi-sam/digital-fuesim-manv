fdd0b6b675b18f9dace886a4345cbb77f0c1e876
anonym-HPI <68286419+anonym-HPI@users.noreply.github.com>
2023-07-07 13:26:26 +0200

    update a lot for license PR

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -4,3 +4,4 @@ Please make sure to fulfil the following conditions before marking this PR ready
 
 -   [ ] If this PR adds or changes features or fixes bugs, this has been added to the changelog
 -   [ ] If this PR adds new actions or other ways to alter the state, [test scenarios](https://github.com/hpi-sam/digital-fuesim-manv-public-test-scenarios) have been added
+-   [ ] I have the right to the code I commited in this Pull Request (this includes that I don't use any AI tools that violate or could violate the license [LICENSE-README.md](LICENSE-README.md) of this project or copied code from e.g. a forum that falls under an incompatible license and if compatible have given credit in the correct way).
---
+++ third-party-license-json-to-html.cjs
@@ -0,0 +1,70 @@
+const json2html = require('node-json2html');
+
+/**
+ * With the help of: https://gist.github.com/joviano-dias/f9bfb8af690a89bcf7d83213e6009b13
+ * and https://www.w3schools.com/html/tryit.asp?filename=tryhtml_table_intro
+ */
+
+let template_table_header = {
+    '<>': 'tr',
+    html: [
+        { '<>': 'th', html: 'Name' },
+        { '<>': 'th', html: 'Url' },
+        { '<>': 'th', html: 'licenseName' },
+        { '<>': 'th', html: 'licenseText' },
+    ],
+};
+
+let template_table_body = {
+    '<>': 'tr',
+    html: [
+        { '<>': 'td', html: '${name}' },
+        { '<>': 'td', html: '${url}' },
+        { '<>': 'td', html: '${licenseName}' },
+        { '<>': 'td', html: '${licenseText}' },
+    ],
+};
+
+let data = require('./frontend/src/assets/source-code/THIRD-PARTY-LICENSE-ACKNOWLEDGEMENTS.json');
+
+let table_header = json2html.transform(data[0], template_table_header);
+let table_body = json2html.transform(data, template_table_body);
+
+let header =
+    '<!DOCTYPE html>' +
+    '<html lang="en">\n' +
+    '<head><title>Third Party Licenses</title></head>';
+let body =
+    '<input type="button" value="Zur&uuml;ck" onclick="history.back()">' +
+    '<h1>Third Party Licenses</h1><br><table id="my_table" style="width:100%">\n<thead>' +
+    table_header +
+    '\n</thead>\n<tbody>\n' +
+    table_body +
+    '\n</tbody>\n</table>';
+body = '<body>' + body + '</body>';
+style = `<style>
+            table {
+                border-collapse: collapse;
+                width: 100%;
+            }
+            th {
+                border: 1px solid #dddddd;
+            }
+            tr:nth-child(even) {
+                background: #dddddd;
+            }
+            tr:nth-child(even) td {
+                border: 1px solid white;
+            }
+            tr:nth-child(odd) td {
+                border: 1px solid #dddddd;
+            }
+        </style>`;
+let html = header + style + body + '</html>';
+
+const fs = require('fs');
+fs.writeFileSync(
+    './frontend/src/assets/source-code/THIRD-PARTY-LICENSE-ACKNOWLEDGEMENTS.html',
+    html
+);
+console.log('THIRD-PARTY-LICENSE-ACKNOWLEDGEMENTS.html successfully created');
