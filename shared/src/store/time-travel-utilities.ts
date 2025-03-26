daf4acde401d126fd434d4c12009551c3e7db4e4
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2025-03-26 12:56:31 +0100

    Update shared dependencies, match frontend immer
    Signed-off-by: Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>

--- shared/src/store/time-travel-utilities.ts
+++ shared/src/store/time-travel-utilities.ts
@@ -1,4 +1,4 @@
-import produce from 'immer';
+import { produce } from 'immer';
 import type { ExerciseState } from '../state';
 import type { Mutable } from '../utils';
 import { sleep } from '../utils';
